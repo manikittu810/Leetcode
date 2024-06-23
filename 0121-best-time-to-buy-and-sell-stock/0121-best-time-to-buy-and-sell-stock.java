e4765ebbde09dfb11bdc4de207b62412337c8d11
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-06-22 17:21:24 -0700

    Time: 1 ms (99.78%), Space: 61.9 MB (19.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maxProfit(int[] a) {
+        int maxProfit =0;
         int l=0,r=0;
-        int max = 0;
-        while(r<a.length){
+        while(r< a.length){
            if(a[l]>a[r]){
             l=r;
            }
            else{
-            max = Math.max(max,a[r]-a[l]);
+            maxProfit=Math.max(maxProfit,a[r]-a[l]);
            }
            r++;
         }
-        return max;
+        return maxProfit;
     }
 }
