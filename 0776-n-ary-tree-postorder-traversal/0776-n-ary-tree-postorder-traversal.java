914b0df5d2db63f59999224e57bf43f37c150a48
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-08-07 16:58:35 -0700

    Time: 0 ms (100%), Space: 44.8 MB (30.73%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    private List<Integer> l;
+    public List<Integer> postorder(Node root) {
+        l = new ArrayList<>();
+        helper(root);
+        return l;
+    }
+    private void helper(Node root ){
+        if(root == null){
+            return;
+        }
+        for(Node child : root.children){
+            helper(child);
+        }
+        l.add(root.val);
+    }
+}
