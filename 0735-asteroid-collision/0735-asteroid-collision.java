866093c3cbf055480988734fbc69d7e99341087e
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-08-27 21:27:20 -0700

    Time: 8 ms (20.11%), Space: 45.4 MB (39.68%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] asteroidCollision(int[] a) {
         Stack<Integer> st = new Stack<>();
-        for(int i : a){
+        for(int i: a){
             if(st.isEmpty() || i>0){
                 st.push(i);
             }
+                    
             else{
-                boolean destroyed = false;;
+                boolean destroyed = false;
+
                 while(!st.isEmpty() && st.peek()>0){
-                    if(st.peek()< Math.abs(i)){
+
+                    if(st.peek() < Math.abs(i)){
                         st.pop();
                     }
                     else if(st.peek()==Math.abs(i)){
@@ -17,6 +20,7 @@ class Solution {
                         break;
                     }
                     else{
+                       
                         destroyed = true;
                         break;
                     }
@@ -25,12 +29,11 @@ class Solution {
                     st.push(i);
                 }
             }
-            
         }
         int ans[] = new int[st.size()];
         for(int i=st.size()-1;i>=0;i--){
             ans[i] = st.pop();
         }
-    return ans;
+        return ans;
     }
 }
