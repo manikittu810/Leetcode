2a480ac7f3a637b6f3433f44ead09b6e80b4ace4
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-08-29 18:50:48 -0700

    Time: 1 ms (96.05%), Space: 44.1 MB (66.78%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,25 +14,27 @@
  * }
  */
 class Solution {
+    Map<Integer,Integer> map = new HashMap<>();
     private int preOrderIndex;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return preOrder(preorder,inorder,0,inorder.length-1);
-    }
-    private TreeNode preOrder(int[]a,int[]b,int startIdx,int endIdx){
-        if(startIdx>endIdx){return null;}
-        TreeNode root = new TreeNode(a[preOrderIndex++]);
+if(preorder.length<1 || inorder.length<1){
+    return null;
+}    
+for(int i=0;i<inorder.length;i++){
+    map.put(inorder[i],i);
+}
+return preOrder(preorder,0,inorder.length-1);
+}
+    private TreeNode preOrder(int[]a,int startIdx,int endIdx){
+        if(preOrderIndex > a.length-1 || startIdx>endIdx){return null;}
 
-        int mid = startIdx;
+        int curVal = a[preOrderIndex++];
+        TreeNode root = new TreeNode(curVal);
 
-        for(int i=startIdx;i<=endIdx;i++){
-            if(b[i] == root.val){
-                mid = i;
-                break;
-            }
-        }
+        int mid = map.get(curVal);
 
-        root.left = preOrder(a,b,startIdx,mid-1);
-        root.right = preOrder(a,b,mid+1,endIdx);
+        root.left = preOrder(a,startIdx,mid-1);
+        root.right = preOrder(a,mid+1,endIdx);
         return root;
     }
 
