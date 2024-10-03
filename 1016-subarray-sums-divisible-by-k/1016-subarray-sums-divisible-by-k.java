4671d580c63b8ab5e3f9ae7c24a6096c9384e95f
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-10-03 10:48:39 -0700

    Time: 19 ms (73.15%), Space: 46.2 MB (79.37%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -4,18 +4,19 @@ class Solution {
         for(int i=0;i<a.length;i++){
             prefixSum[i+1] = prefixSum[i]+a[i];
         }
-        int count = 0;
+
+        int count =0;
         Map<Integer,Integer> map = new HashMap<>();
-        map.put(0,1);
-        for(int i=1;i<prefixSum.length;i++){
+        // map.put(0,1);
+        for(int i=0;i<prefixSum.length;i++){
             int remainder = prefixSum[i]%k;
-            if(remainder<0){
+            if(remainder <0){
                 remainder+=k;
             }
             if(map.containsKey(remainder)){
-                 count+=map.get(remainder);
+                count+=map.get(remainder);
             }
-            map.put(remainder, map.getOrDefault(remainder,0)+1);
+            map.put(remainder,map.getOrDefault(remainder,0)+1);
         }
         return count;
     }
