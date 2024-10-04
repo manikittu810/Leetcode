4650c292f7639163ec54b9ea82d043db5d01cbfb
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-10-03 21:33:03 -0700

    Time: 1037 ms (5.54%), Space: 61.6 MB (5.29%) - LeetHub

---
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
@@ -0,0 +1,43 @@
+class Solution {
+    public long dividePlayers(int[] a) {
+        if(a.length%2!=0){
+            return -1;
+        }
+        Arrays.sort(a);
+
+        int l=0,r=a.length-1;
+        long sum=0, chem=0;
+
+        Map<long[],Long> map = new HashMap<>();
+
+        while(l<r){
+        long []b = new long[2];
+            sum=0;
+            b[0]=a[l];
+            b[1]=a[r];
+            l++;
+            r--;
+            sum = b[0]+b[1];
+            map.put(b,sum);
+        }
+        for(Map.Entry<long[],Long> entry : map.entrySet()){
+            long v = entry.getValue();
+            if(v!=sum){
+                return -1;
+            }
+            else{
+                long []key = entry.getKey();
+                for(int i=0;i<2;i++){
+                    System.out.print(key[i] + " ");
+                }
+                System.out.print(" : "+v);
+                long []c = new long[2];
+                c[0] = key[0];
+                c[1] = key[1];
+                chem+= c[0]*c[1];
+            }
+            System.out.println();
+        }
+        return chem;
+    }
+}
