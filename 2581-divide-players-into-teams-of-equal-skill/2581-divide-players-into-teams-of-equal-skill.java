07a39c538b8e65436ca3f2a8c34c1a2e8f9822d1
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-10-03 21:36:47 -0700

    Time: 52 ms (5.54%), Space: 58.2 MB (10.08%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
@@ -1,16 +1,17 @@
 class Solution {
     public long dividePlayers(int[] a) {
+
         if(a.length%2!=0){
             return -1;
         }
-        Arrays.sort(a);
+        Arrays.sort(a);//log n
 
         int l=0,r=a.length-1;
         long sum=0, chem=0;
 
-        Map<long[],Long> map = new HashMap<>();
+        Map<long[],Long> map = new HashMap<>();//O(n) space
 
-        while(l<r){
+        while(l<r){//O(n) tc
         long []b = new long[2];
             sum=0;
             b[0]=a[l];
@@ -20,23 +21,23 @@ class Solution {
             sum = b[0]+b[1];
             map.put(b,sum);
         }
-        for(Map.Entry<long[],Long> entry : map.entrySet()){
+        for(Map.Entry<long[],Long> entry : map.entrySet()){//O(n)
             long v = entry.getValue();
             if(v!=sum){
                 return -1;
             }
             else{
                 long []key = entry.getKey();
-                for(int i=0;i<2;i++){
-                    System.out.print(key[i] + " ");
-                }
-                System.out.print(" : "+v);
+                // for(int i=0;i<2;i++){
+                //     System.out.print(key[i] + " ");
+                // }
+                // System.out.print(" : "+v);
                 long []c = new long[2];
                 c[0] = key[0];
                 c[1] = key[1];
                 chem+= c[0]*c[1];
             }
-            System.out.println();
+            // System.out.println();
         }
         return chem;
     }
