7c7a7fc01003cf520300e465491197b9d52ae5fb
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-10-29 10:29:15 -0700

    Time: 1 ms (80.95%), Space: 41.3 MB (40.28%) - LeetHub

--- 2412-minimum-amount-of-time-to-fill-cups/2412-minimum-amount-of-time-to-fill-cups.java
+++ 2412-minimum-amount-of-time-to-fill-cups/2412-minimum-amount-of-time-to-fill-cups.java
@@ -1,26 +1,22 @@
 class Solution {
     public int fillCups(int[] a) {
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<>((b,c) -> c-b);
-        for(int i : a){
-            if(i>0){
-            maxHeap.offer(i);
-            }
-        }
         int c = 0;
-        while(maxHeap.size()>1){
-            int n1 = maxHeap.poll()-1;
-            int n2 = maxHeap.poll()-1;
-            if(n1>0){
-                maxHeap.offer(n1);
+        while(true){
+            Arrays.sort(a);
+            if(a[2] == 0){
+                break;
             }
-            if(n2>0){
-                maxHeap.offer(n2);
+
+            if(a[1]>0){
+                a[1]--;
+                a[2]--;
+            }
+            else{
+                a[2]--;
             }
             c++;
         }
-        if(!maxHeap.isEmpty()){
-            c+=maxHeap.poll();
-        }
         return c;
+        
     }
 }
