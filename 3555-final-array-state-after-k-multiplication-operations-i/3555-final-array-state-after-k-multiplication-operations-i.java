a27f7d0f279dddfa37605ccc4712d482ff941a8d
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-11-01 18:22:38 -0700

    Time: 3 ms (35.35%), Space: 45 MB (11.55%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,53 +1,18 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        int res[] = new int[nums.length];
-
-        PriorityQueue<Pair<Integer,Integer>> pq = new PriorityQueue<>();
-
-        for(int i=0;i<nums.length;i++){
-            pq.offer(new Pair<>(nums[i],i));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
+        for(int i=0; i<nums.length; i++){
+            pq.offer(new int[]{nums[i], i});
         }
-
-        for(int i=0;i<k;i++){
-            Pair<Integer,Integer> p = pq.poll();
-            int f = p.getFirst();
-            int s = p.getSecond();
-            int product = f*multiplier;
-            pq.offer(new Pair<>(product,s));
+        
+        for(int i=0; i<k; i++){
+            int[] top = pq.remove();
+            
+            int new_num = ((top[0]*multiplier));
+            nums[top[1]] = new_num;
+            
+            pq.offer(new int[]{new_num, top[1]});
         }
-        while(!pq.isEmpty()){
-            Pair<Integer,Integer> p = pq.poll();
-            int firstNum = p.getFirst();
-            int idx = p.getSecond();
-            res[idx] = firstNum;
-        }
-        return res;
-    }
-}
-
-class Pair<U extends Comparable<U>, V extends Comparable<V>> implements Comparable<Pair<U, V>> {
-    final U first;
-    final V second;
-
-    Pair(U first, V second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    public U getFirst() {
-        return first;
-    }
-
-    public V getSecond() {
-        return second;
-    }
-
-    @Override
-    public int compareTo(Pair<U, V> other) {
-        int comp = this.first.compareTo(other.first);
-        if(comp==0){
-            return this.second.compareTo(other.second);
-        }
-        return comp;
+        return nums;
     }
 }
