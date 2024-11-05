588b5d1240213b09bb9ba128c28d365c69b88aa3
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-11-04 16:19:12 -0800

    Time: 14 ms (57.9%), Space: 48.8 MB (32.47%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,51 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+    public int[] topKFrequent(int[] a, int k) {
+     Map<Integer,Integer> map = new HashMap<>();
+        for(int i : a){
+                map.put(i,map.getOrDefault(i,0)+1);
         }
 
-        PriorityQueue<Map.Entry<Integer,Integer>> minHeap = new PriorityQueue<>((a,b) -> a.getValue()-b.getValue());
+        PriorityQueue<Pair<Integer,Integer>> minHeap = new PriorityQueue<>((b,c) ->{
+            if(c.getFirst().equals(b.getFirst())){
+                return b.getSecond() - c.getSecond();
+            }
+            return b.getFirst() -c.getFirst();
+        });
 
         for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            minHeap.add(entry);
+
+            int key = entry.getKey();
+           int val = entry.getValue();
+
+            minHeap.offer(new Pair<>(val, key));
+
             if(minHeap.size()>k){
                 minHeap.poll();
             }
         }
-        int []a = new int[k];
+        int []b = new int[k];
         for(int i=0;i<k;i++){
-            a[i] = minHeap.poll().getKey();
+            b[i] = minHeap.poll().getSecond();
         }
-return a;
+        return b;
     }
+}
+class Pair<U extends Comparable<U>,V extends Comparable <V>> implements Comparable<Pair<U,V>>{
+    final  U first;
+    final  V second;
+    Pair(U first,V second){
+        this.first=first;
+        this.second=second;
+    }
+    public U getFirst(){
+        return first;
+    }
+    public V getSecond(){
+        return second;
+    }
+    @Override
+    public int compareTo(Pair<U,V> other){
+        return other.first.compareTo(this.first);
+    }
+
 }
