1955c3b7ba41f20c06e0a8053d6f9168548f1d81
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2024-11-26 23:53:32 -0800

    Time: 3 ms (13.94%), Space: 44.5 MB (9.89%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,27 +1,30 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-
         List<String> l = new ArrayList<>();
-        if(n == 0){
-            return l;
-        }
-        backTrack(l,n,new StringBuilder(),0,0);
+        Stack<Character> st = new Stack<>();
+        backTrack(l,n,n,n,st);
         return l;
     }
-    private void backTrack(List<String> l,int n,StringBuilder sb, int openCount,int closedCount){
-        if(sb.length() == 2*n){
+    private void backTrack(List<String> l,
+    int n, int openBracket,int closedBracket,Stack<Character> st){
+        if(st.size() == 2*n){
+            StringBuilder sb = new StringBuilder();
+            for(Character c : st){
+                sb.append(c);
+            }
             l.add(sb.toString());
-            return;
+            return ;
         }
-    if(openCount < n){
-        sb.append('(');
-        backTrack(l,n,sb,openCount+1,closedCount);
-        sb.deleteCharAt(sb.length()-1);
+        if(openBracket>0){
+            st.push('(');
+            backTrack(l,n,openBracket-1,closedBracket,st);
+            st.pop();
+        }
+        if(closedBracket>openBracket){
+            st.push(')');
+            backTrack(l,n,openBracket,closedBracket-1,st);
+            st.pop();
+        }
     }
-    if(closedCount < openCount){
-        sb.append(')');
-        backTrack(l,n,sb,openCount,closedCount+1);
-        sb.deleteCharAt(sb.length()-1);
-    }
-    }
+    
 }
