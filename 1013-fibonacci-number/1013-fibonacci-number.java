ac3925f8231b415bfb425b05dd46b27e28f721b9
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2024-12-26 17:55:38 -0800

    Time: 0 ms (100%), Space: 40.5 MB (40.78%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,14 @@
 class Solution {
+    private Map<Integer,Integer> cache = new HashMap<>();
     public int fib(int n) {
         if(n<=1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
-        
+        if(cache.containsKey(n)){
+            return cache.get(n);
+        }
+       int res = fib(n-1)+fib(n-2);
+       cache.put(n,res);
+       return res;
     }
 }
