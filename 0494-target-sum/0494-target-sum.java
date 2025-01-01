d13a2ec659a5d6b689faecf94d122201988772df
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2024-12-31 22:46:28 -0800

    Time: 3 ms (94.99%), Space: 41.4 MB (64.15%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,29 +1,22 @@
 class Solution {
-    public int findTargetSumWays(int[] a, int d) {
-        int n = a.length;
-        int sum=0;
-        for(int i : a){
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        int sum =0;
+        for(int i : nums){
             sum+=i;
         }
-
-        int sum1 = (sum+d)/2;
-        if((sum+d)%2!=0 || Math.abs(d)>sum){
+        int sum1 = (sum+target)/2;
+        if(Math.abs(target)>sum || (sum+target)%2!=0){
             return 0;
         }
 
-        int[][]dp = new int[n+1][sum1+1];
-        dp[0][0]=1;
-
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=sum1;j++){
-                if(j>=a[i-1]){
-                    dp[i][j]=dp[i-1][j-a[i-1]]+dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
+        int[]dp = new int[sum+1];
+        dp[0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=sum1;j>=nums[i];j--){
+                dp[j]+=dp[j-nums[i]];
             }
         }
-        return dp[n][sum1];
+        return dp[sum1];
     }
 }
