3373d660ef36580a44499cee43eca9ee4480b518
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-01-03 00:02:05 -0800

    Time: 2 ms (100%), Space: 43.9 MB (98.07%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.java
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.java
@@ -11,36 +11,29 @@ class Solution {
             for(int j=0;j<col;j++){
                 if(grid[i][j] == 0 &&
                 (i==0 || i==row-1||j==0||j==col-1)){
-                    bfs(grid,i,j,directions);
+                    dfs(grid,i,j);
                 }
             }
         }
         for(int i =0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]==0){
-                    bfs(grid,i,j,directions);
+                    dfs(grid,i,j);
                     count++;
                 }
             }
         }
         return count;
     }
-    private void bfs(int[][]grid,int i,int j,int[][]directions){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{i,j});
-        grid[i][j] = 1;
-        while(!q.isEmpty()){
-            int n = q.size();
-            int []u = q.poll();
-            for(int []k:directions){
-                int newRow = u[0]+k[0];
-                int newCol = k[1]+u[1];
-                if(newRow>=0 && newCol>=0
-                && newRow<grid.length && newCol<grid[0].length &&grid[newRow][newCol]==0){
-                    grid[newRow][newCol] = 1;
-                    q.offer(new int[]{newRow,newCol});
+    private void dfs(int[][]grid,int i,int j){
+                if(i<0 || j<0
+            || i>=grid.length || j>=grid[0].length ||grid[i][j]==1){
+                    return;
                 }
-            }
-        }
-    }
+                grid[i][j]=1;
+                dfs(grid,i+1,j);
+                dfs(grid,i-1,j);
+                dfs(grid,i,j+1);
+                dfs(grid,i,j-1);
+}
 }
