b62d72696637926f1f21e647a7c3a1bcd4824292
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-01-12 09:46:43 -0800

    Time: 0 ms (100%), Space: 41 MB (68.09%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -10,13 +10,15 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode sptr = head;
-        ListNode fptr = head;
-
-        while(fptr!=null&& fptr.next!=null){
-            sptr =sptr.next;
-            fptr = fptr.next.next;
+        int count = 0;
+        for(ListNode cur=head;cur!=null;cur =cur.next){
+            count++;
         }
-        return sptr;
-    }
+        ListNode cur = head;
+       
+        for(int i=0;i<count/2;i++){
+            cur = cur.next;//if count is odd
+        }   
+        return cur; 
+}
 }
