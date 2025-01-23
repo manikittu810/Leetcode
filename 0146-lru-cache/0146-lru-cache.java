f7affeb77ed5a42459429fd26283c8bf5edf72bc
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-01-22 22:20:03 -0800

    Time: 45 ms (70.94%), Space: 112.8 MB (77.91%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,19 +1,21 @@
 class LRUCache {
 
     class Node{
-            int key,val;
-            Node prev,next;
-            Node(int key,int val){
-                this.key = key;
-                this.val = val;
-            }
+        int key,val;
+        Node prev,next;
+        Node(int key,int val){
+            this.key = key;
+            this.val = val;
         }
-        private Node head;
-        private Node tail;
-        private Map<Integer,Node> map;
-        private int capacity;
+    }
+    private Node head;
+    private Node tail;
+    private Map<Integer,Node> map;
+    private int capacity;
+
 
     public LRUCache(int capacity) {
+
         this.capacity = capacity;
         map = new HashMap<>();
         head = new Node(0,0);
@@ -23,60 +25,46 @@ class LRUCache {
     }
     
     public int get(int key) {
-
         Node node = map.get(key);
-
-        if(node == null){
+        if( node == null){
             return -1;
         }
-
         removeNode(node);
-
         addAtHead(node);
-
         return node.val;
     }
     
     public void put(int key, int value) {
-
-       if(map.containsKey(key)){
-
-        Node node = map.get(key);
-        node.val = value;
-
-        removeNode(node);
-
-        addAtHead(node);
-
-       }else{
-
-        if(map.size()==capacity){
-
-            Node lru = removeAtTail();
-            map.remove(lru.key);
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            node.val = value;
+            removeNode(node);
+            addAtHead(node);
+        }else{
+            if(map.size()==capacity){
+                Node node = removeAtTail();
+                map.remove(node.key);
+            }
+            Node node = new Node(key,value);
+            addAtHead(node);
+            map.put(key,node);
         }
-
-       Node newNode  = new Node(key,value);
-       addAtHead(newNode);
-       map.put(key,newNode);
-       }
+        
     }
-
     public void addAtHead(Node node){
         node.next = head.next;
-        head.next.prev = node;
-        node.prev = head;
+        node.next.prev = node;
         head.next = node;
+        node.prev = head;
     }
-
     public Node removeAtTail(){
         Node lru = tail.prev;
         removeNode(lru);
         return lru;
     }
     public void removeNode(Node node){
-        node.next.prev = node.prev;
         node.prev.next = node.next;
+        node.next.prev = node.prev;
     }
 }
 
