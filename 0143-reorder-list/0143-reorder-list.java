5fa8be803003e77164c996b9a893d65ce7ef38df
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-01-23 18:21:06 -0800

    Time: 3 ms (20.96%), Space: 48 MB (79.47%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,6 +10,9 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
+        if(head == null || head.next == null){
+            return;
+        }
 
         ListNode slow = head;
         ListNode fast = head;
