5c1ef0a226bcfe111c92be11c59f86140ef0d70a
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-01-27 17:23:03 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,36 +1,47 @@
 class Solution {
-    public int[] findOrder(int V, int[][] grid) {
-        List<List<Integer>> adjList = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
-        int[] inDegree = new int[V];
+    public int[] findOrder(int V, int[][] a) {
+
+        List<List<Integer>> l = new ArrayList<>();
+
         for(int i=0;i<V;i++){
-            adjList.add(new ArrayList<>());
+
+            l.add(new ArrayList<>());
+
         }
-        for(int i=0;i<grid.length;i++){
-            int u = grid[i][1];
-            int v = grid[i][0];
-            adjList.get(u).add(v);
+
+        int[]inDegree = new int[V];
+
+        for(int i=0;i<a.length;i++){
+
+            int u = a[i][1];
+
+            int v = a[i][0];
+
+            l.get(u).add(v);
+
             inDegree[v]++;
+
         }
+
+        Queue<Integer> q = new LinkedList<>();
 
         for(int i=0;i<V;i++){
             if(inDegree[i]==0){
                 q.offer(i);
             }
         }
-        int [] res = new int[V];
-        int idx = 0;
+            int idx = 0;
+            int []res = new int[V];
         while(!q.isEmpty()){
-            int u = q.poll();
+            int u= q.poll();
             res[idx++] = u;
-            for(int i : adjList.get(u)){
-                inDegree[i]--;
-                if(inDegree[i]==0){
-                    q.offer(i);
+            for(int v : l.get(u)){
+                inDegree[v]--;
+                if(inDegree[v]==0){
+                    q.offer(v);
                 }
             }
         }
-
-        return (idx == V) ? res : new int[0];
+        return res;
     }
 }
