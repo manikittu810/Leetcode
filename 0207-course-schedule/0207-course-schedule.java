698d2a776425bf2d10c52f7c8611f37dcd7c6f13
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-01-27 16:51:45 -0800

    Time: 6 ms (74.37%), Space: 45.6 MB (50.12%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,21 +1,26 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] grid) {
-        List<List<Integer>> adjList = new ArrayList<>();
-        Queue<Integer> q = new ArrayDeque<>();
-        int[] inDegrees = new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            adjList.add(new ArrayList<>());//[[],[]]
-        }
-        for(int i=0;i<grid.length;i++){
-            int u = grid[i][0];
-            int v = grid[i][1];
-            adjList.get(v).add(u);
-            inDegrees[u]++;
+    public boolean canFinish(int V, int[][] a) {
+        List<List<Integer>> l = new ArrayList<>();
 
+        for(int i=0;i<V;i++){
+           l.add(new ArrayList<>());
+        }
+        int[] inDegree= new int[V];
+
+        for(int i=0;i<a.length;i++){
+            int u = a[i][1];
+
+            int v = a[i][0];
+
+            l.get(u).add(v);
+
+            inDegree[v]++;
         }
 
-        for(int i=0;i<numCourses;i++){
-            if(inDegrees[i] == 0){
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0;i<V;i++){
+            if(inDegree[i]==0){
                 q.offer(i);
             }
         }
@@ -23,17 +28,26 @@ class Solution {
 
         while(!q.isEmpty()){
             int u = q.poll();
-            count++;
-                for(int v : adjList.get(u)){
-                    inDegrees[v]--;
-                    if(inDegrees[v]==0){
-                        q.offer(v);
-                    }
+
+            for(int v : l.get(u)){
+
+                inDegree[v]--;
+
+                if(inDegree[v]==0){
+                    q.offer(v);
                 }
             }
-        if(count!=numCourses){
-            return false;
+
+            count++;
         }
-        return true;
+        return (count!=V) ? false : true;
     }
 }
+/*
+given two vertices 
+given [1,0]
+0->1
+0 is the job and 1 is the dependency
+i need to solve 0 before visiting 1
+count == V true else false
+ */
