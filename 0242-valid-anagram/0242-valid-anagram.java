8cf758c560d1e64514d5df20d7aaa801983186d1
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-01-28 23:27:16 -0800

    Time: 27 ms (5.73%), Space: 44.7 MB (45.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,19 +3,12 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int []count = new int[26];
-
+        Map<Character , Integer> map1 = new HashMap<>();
+        Map<Character , Integer> map2 = new HashMap<>();
         for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
+            map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0)+1);
+            map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
         }
-        for(int i=0;i<t.length();i++){
-            count[t.charAt(i)-'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(count[i]!=0){
-                return false;
-            }
-        }
-        return true;
+        return map1.equals(map2);
     }
 }
