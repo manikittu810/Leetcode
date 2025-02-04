48d88f5b5f7128234689904ad2151c05b81a462b
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-03 20:02:54 -0800

    Time: 15 ms (38.41%), Space: 48.7 MB (52.96%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,54 +1,48 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i :nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
 
-        PriorityQueue<Pair<Integer,Integer>> pq = new PriorityQueue<>((b,c) -> {
-
+        PriorityQueue<Pair<Integer,Integer>> minHeap = new PriorityQueue<>((b,c) ->{
             if(b.getFirst().equals(c.getFirst())){
                 return b.getSecond() - c.getSecond();
             }
             return b.getFirst() - c.getFirst();
         });
 
-        Map<Integer,Integer> map = new HashMap<>();
-
-        for(int i : nums){
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-
         for(Map.Entry<Integer,Integer> entry : map.entrySet()){
             int key = entry.getKey();
-            int value = entry.getValue();
+            int val = entry.getValue();
 
-            pq.offer(new Pair(value,key));
+            minHeap.offer(new Pair(val,key));
 
-            while(pq.size()>k){
-                pq.poll();
+            if(minHeap.size()>k){
+                minHeap.poll();
             }
         }
-
-        int []res = new int[k];
-       for(int i=0;i<k;i++){
-        res[i] = pq.poll().getSecond();
-       }
-        return res;
+    int[] res = new int[k];
+    for(int i=0;i<k;i++){
+        res[i] = minHeap.poll().getSecond();
     }
-    
+return res;
+        
+    }
 }
-public class Pair <U extends Comparable<U>, V extends Comparable<V>> implements Comparable<Pair<U,V>>{
-        private final  U  first;
-        private final  V  second;
-        Pair(U first,V second){
-            this.first = first;
-            this.second = second;
-        }
-        public U getFirst(){
-            return first;
-        }
-       public  V getSecond(){
-            return  second;
-        }
-        @Override
-        public int compareTo(Pair<U,V> otherPair){
-            return this.first.compareTo(otherPair.first);
-        }
+public class Pair<U extends Comparable<U>,V extends Comparable<V>> implements Comparable<Pair<U,V>>{
+    private final U first;
+    private final V second;
+    Pair(U first,V second){
+        this.first = first;
+        this.second = second;
     }
+
+    public U getFirst(){return first;}
+    public V getSecond(){return second;}
+
+    @Override
+    public int compareTo(Pair<U,V> other){
+        return this.first.compareTo(other.first);
+    }
+}
