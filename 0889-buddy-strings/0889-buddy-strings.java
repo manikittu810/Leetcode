e98a3f132ddf4007ee40daad3251a2d02eed063b
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-04 18:25:39 -0800

    Time: 3 ms (56.78%), Space: 42.3 MB (87.67%) - LeetHub

---
+++ 0889-buddy-strings/0889-buddy-strings.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean buddyStrings(String s, String g) {
+        if(s.length()!=g.length()){return false;}
+            Set<Character> set = new HashSet<>();
+
+        if(s.equals(g)){
+            for(Character c :s.toCharArray()){
+                if(!set.add(c)){
+                    return true;
+                }
+            }
+            return  false;
+        }
+
+        List<Integer> l = new ArrayList<>();
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) != g.charAt(i)){
+                l.add(i);
+            }
+        }
+
+        if(l.size()==2){
+            int i = l.get(0);
+            int j = l.get(1);
+
+            return(s.charAt(i)==g.charAt(j) && s.charAt(j)==g.charAt(i));
+        }
+        return false;
+    }
+}
