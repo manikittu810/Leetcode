423877f2559e10e7f4a33e6d33e21a7b2bae1f78
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-08 00:02:24 -0800

    Time: 60 ms (5.58%), Space: 98.3 MB (30.9%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,43 +1,37 @@
-
 class Solution {
     public int[] queryResults(int limit, int[][] a) {
-        Map<Integer, Integer> map = new HashMap<>(); // Ball -> Color
-        Map<Integer, Integer> colorCount = new HashMap<>(); // Color -> Count
-        Set<Integer> set = new HashSet<>(); // Unique colors
-        int[] res = new int[a.length];
+        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer,Integer> colorCount = new HashMap<>();
 
-        for (int i = 0; i < a.length; i++) {
-            int x = a[i][0]; 
-            int y = a[i][1]; 
+        Set<Integer> set = new HashSet<>();
+        int[]res = new int[a.length];
 
-            if (map.containsKey(x)) {
-                int oldColor = map.get(x);
-                if (oldColor != y) {
-                    colorCount.put(oldColor, colorCount.getOrDefault(oldColor, 0) - 1);
-                    
-                    // If no balls use the old color, remove it from set
-                    if (colorCount.get(oldColor) == 0) {
-                        set.remove(oldColor);
+        for(int i=0;i<a.length;i++){
+            int x=a[i][0];
+            int y =a[i][1];
+
+            if(!map.containsKey(x)){
+
+                map.put(x,y);
+
+            }else {
+                int oldBall = map.get(x);
+                if(oldBall!=y){
+                    colorCount.put(oldBall,colorCount.getOrDefault(oldBall,0)-1);
+                    if(colorCount.get(oldBall)==0){
+                        set.remove(oldBall);
                     }
-                    
-                    // Assign new color
-                    map.put(x, y);
-                } else {
-                    // No change in color, just store the result
-                    res[i] = set.size();
-                    continue;
-                }
-            } else {
-                // Assign the ball its new color
-                map.put(x, y);
+                    map.put(x,y);
+               }else{
+                res[i] = set.size();
+                continue;
+               }
             }
-
-            colorCount.put(y, colorCount.getOrDefault(y, 0) + 1);
+            colorCount.put(y,colorCount.getOrDefault(y,0)+1);
             set.add(y);
-
             res[i] = set.size();
         }
+            return res;
 
-        return res;
-    }
+}
 }
