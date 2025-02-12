13f1a99e0e98c62c3e8abc6379e14fd3873ddcb7
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-11 18:47:52 -0800

    Time: 79 ms (22.5%), Space: 69.2 MB (7.73%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -2,48 +2,32 @@ class Solution {
     public int maximumSum(int[] a) {
         Map<Integer,int[]> map = new HashMap<>();
         int maxVal = Integer.MIN_VALUE;
-        for(int i: a){
-
-        int key = calculateSum(i);
-
-        if(!map.containsKey(key)){
-
-            map.put(key,new int[]{i,-1});
-
+        for(int i : a){
+        int sum = claculateDigitSum(i);
+        if(!map.containsKey(sum)){
+            map.put(sum,new int[]{i,-1});
         }else{
-
-            int[]topTwoNums = map.get(key);//18
-
-            int firstNum = topTwoNums[0];
-            int secondNum = topTwoNums[1];
-
-            if(firstNum<i){
-
-                secondNum = firstNum;
-                firstNum = i;
-
-            }else if(secondNum <i){
-
-                secondNum = i;
+            int[]topTwoNums = map.get(sum);
+            if(i > topTwoNums[0]){
+                topTwoNums[1] = topTwoNums[0];
+                topTwoNums[0]=i;
+            }else if(topTwoNums[1] < i){
+                topTwoNums[1] = i;
             }
-            map.put(key,new int[]{firstNum,secondNum});
-
-            if(secondNum!=-1){
-                maxVal = Math.max(maxVal,firstNum+secondNum);
+            map.put(i, new int[]{topTwoNums[0],topTwoNums[1]});
+            if(topTwoNums[1]!=-1){
+                maxVal = Math.max(maxVal,topTwoNums[0]+topTwoNums[1]);
             }
         }
-        }
-        return (maxVal == Integer.MIN_VALUE) ? -1:maxVal;
-
+        }        
+        return (maxVal==Integer.MIN_VALUE) ? -1 : maxVal;
     }
-
-    private int calculateSum(int n){
-        int sum = 0;
+    private int claculateDigitSum(int n){
+        int sum =0;
         while(n>0){
             sum+=n%10;
             n/=10;
         }
         return sum;
     }
-
 }
