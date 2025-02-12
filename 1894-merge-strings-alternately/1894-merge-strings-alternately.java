c6943227794baf5893cd22a5aefe108f9df199d8
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-11 23:41:35 -0800

    Time: 0 ms (100%), Space: 42 MB (46.56%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String mergeAlternately(String s1, String s2) {
+
+        int n = s1.length(),m=s2.length();
+
+        char []ch1 = s1.toCharArray();
+        char []ch2 = s2.toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+        int l=0,r=0;
+
+        while(l<n && r<m){
+            sb.append(ch1[l++]);
+            sb.append(ch2[r++]);
+        }
+        while(l<n){
+            sb.append(ch1[l++]);
+        }
+        while(r<m){
+            sb.append(ch2[r++]);
+        }
+        return sb.toString();
+    }
+}
+/*
+
+*/
