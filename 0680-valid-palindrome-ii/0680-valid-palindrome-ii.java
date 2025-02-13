3111c2632a9fbce0cb9234a06e68f7155e53e732
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-12 22:30:20 -0800

    Time: 4 ms (99.71%), Space: 45.2 MB (89.67%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int n = s.length();
-        return helper(s,0,n-1,0);
+        return twoPointerHelper(s,0,s.length()-1,0);
     }
-    private static boolean helper(String s , int l, int r, int count){
-        if(count > 1){return false;}
-        while(l<=r){
+    private boolean twoPointerHelper(String s, int l, int r, int count){
+        if(count>1){
+            return false;
+        }
+        while(l<r){
             if(s.charAt(l)!=s.charAt(r)){
-                return (helper(s,l+1,r,count+1)||helper(s,l,r-1,count+1));
-            }l++;r--;
+                 return (twoPointerHelper(s,l+1,r,count+1)||twoPointerHelper(s,l,r-1,count+1));
+            }
+            l++;
+            r--;
         }
         return true;
     }
