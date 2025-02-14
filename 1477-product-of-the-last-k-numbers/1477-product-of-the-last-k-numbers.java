31e8181cea2b8fd0fa90e1cfc5f4325f29c75014
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-13 22:47:46 -0800

    Time: 245 ms (15.28%), Space: 68.9 MB (53.7%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
@@ -1,24 +1,23 @@
 class ProductOfNumbers {
-    private  List<Integer> prefixProductCache;
-
+    private List<Integer> prefixProductCache;
     public ProductOfNumbers() {
         prefixProductCache = new ArrayList<>();
         prefixProductCache.add(1);
     }
     
     public void add(int num) {
-        if(num==0){
+        if(num == 0){
             prefixProductCache = new ArrayList<>();
             prefixProductCache.add(1);
         }else{
-            int lastCacheValue = prefixProductCache.get(prefixProductCache.size()-1);
-            prefixProductCache.add(lastCacheValue * num);
+            int lastNum = prefixProductCache.get(prefixProductCache.size()-1);
+            prefixProductCache.add(lastNum * num);
         }
     }
     
     public int getProduct(int k) {
         int n = prefixProductCache.size();
-        if(k>=prefixProductCache.size()){
+        if(k>=n){
             return 0;
         }else{
             return prefixProductCache.get(n-1)/prefixProductCache.get(n-1-k);
