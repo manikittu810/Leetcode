7f9e43fa5f247c9a136fc3f4c9eade032ae64e68
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-14 19:20:22 -0800

    Time: 30 ms (68.26%), Space: 51.5 MB (59.91%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] a) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(a);   
-        for(int i = 0;i<a.length-2;i++){
-            if(i>0 && a[i]==a[i-1]) continue;
-           twoSum(a[i],a,i+1,a.length-1,list);
-            }
-        return list;
+        List<List<Integer>> l = new ArrayList<>();
+        Arrays.sort(a);
+        for(int i=0;i<a.length-2;i++){
+            if(i>0 && a[i] == a[i-1]) continue;
+            twoSum(a,a[i],i+1,a.length-1,l);
+        }
+        return l;
     }
-    private void twoSum(int target,int[]a,int l,int r,List<List<Integer>>list){
+    private void twoSum(int[]a,int startNum,int l,int r,List<List<Integer>> list){
         while(l<r){
-            int sum = a[l]+a[r]+target;
-            if(sum == 0 ){
-                list.add(Arrays.asList(target,a[l],a[r]));
-                while(l<r && a[l]==a[l+1]){
-                    l++;
-                }
-                while(l<r && a[r]==a[r-1]){
-                    r--;
-                }
+        int sum = startNum+a[l]+a[r];
+            if(sum == 0){
+
+                list.add(Arrays.asList(startNum,a[l],a[r]));
+
+                while(l<r && a[l]==a[l+1]) l++;
+                while(l<r && a[r]==a[r-1]) r--;
+
                 l++;
                 r--;
-            }else if(sum <  0){
+            }else if(sum<0){
                 l++;
             }else{
                 r--;
