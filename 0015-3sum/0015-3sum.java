353de0df78c0d384965c53437a79cae82f9f7524
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-14 19:03:42 -0800

    Time: 30 ms (68.26%), Space: 51.5 MB (71.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] a) {
         List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(a);
-        for(int i=0;i<a.length-2;i++){
-            if(i>0 && a[i]==a[i-1]){
-                continue;
+        Arrays.sort(a);   
+        for(int i = 0;i<a.length-2;i++){
+            if(i>0 && a[i]==a[i-1]) continue;
+           twoSum(a[i],a,i+1,a.length-1,list);
             }
-
-            int l = i+1, r= a.length-1;
-
-            while(l<r){
-                int sum = a[i]+a[l]+a[r];
-                if(sum ==0){
-                    list.add(Arrays.asList(a[i],a[l],a[r]));
-                    while(l<r && a[l]==a[l+1]) l++;
-                    while(l<r && a[r] == a[r-1]) r--;
-
+        return list;
+    }
+    private void twoSum(int target,int[]a,int l,int r,List<List<Integer>>list){
+        while(l<r){
+            int sum = a[l]+a[r]+target;
+            if(sum == 0 ){
+                list.add(Arrays.asList(target,a[l],a[r]));
+                while(l<r && a[l]==a[l+1]){
                     l++;
+                }
+                while(l<r && a[r]==a[r-1]){
                     r--;
                 }
-                else if(sum<0) {l++;}
-                else{r--;}
+                l++;
+                r--;
+            }else if(sum <  0){
+                l++;
+            }else{
+                r--;
             }
         }
-return list;
     }
 }
