28cd63c326bc0c0b1e0c9a12d26f65ac6217c401
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-17 20:56:55 -0800

    Time: 2 ms (6.95%), Space: 41.1 MB (43.38%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.java
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.java
@@ -1,54 +1,34 @@
-
 class Solution {
     public int[] constructDistancedSequence(int n) {
-
-		int len = 2*n-1;
-
-		int []res = new int[len];
-
-		boolean [] placed = new boolean[n+1];
-
-		backTrack(res,placed,n,0);
-
-		return res;
-
+        int len = 2*n-1;
+        int[] res = new int[len];
+        boolean []placed = new boolean[n+1];
+        backTrack(res,placed,n,0);
+        return res;
     }
-
-	private boolean  backTrack(int[] res, boolean[] placed, int n, int idx){
-
-			if(idx == res.length){
-
-					return true;
-			}
-
-			if(res[idx]!=0){
-				return backTrack(res,placed, n,idx+1);
-			}
-
-			for(int curNum = n;curNum>=1;curNum--){
-
-				if(!placed[curNum]){
-	
-					int secondIdx = (curNum==1) ? idx : idx+curNum;
-
-					if(secondIdx <res.length && res[secondIdx]==0){
-						res[idx] = curNum;
-						res[secondIdx] = curNum;
-						placed[curNum] = true;
-
-						if(backTrack(res,placed,n,idx+1)){
-							return true;
-						}
-
-						res[idx] = 0;
-						res[secondIdx] = 0;
-						placed[curNum] = false;
-					}
-				}
-                	
-			}
-
-		return false;
-
-	}
+    private boolean backTrack(int[] res, boolean[] placed, int n , int idx){
+        if(idx == res.length){
+            return true;
+        }
+        if(res[idx]!=0){
+            return backTrack(res,placed,n,idx+1);
+        }
+        for(int num = n; num>=1;num--){
+            if(!placed[num]){
+                int secondIdx = (num == 1) ? idx : num+idx;
+                if(secondIdx < res.length && res[secondIdx]==0){
+                    res[idx] = num;
+                    res[secondIdx] = num;
+                    placed[num] = true;
+                    if(backTrack(res,placed,n,idx+1)){
+                        return true;
+                    }
+                    res[idx] = 0;
+                    res[secondIdx] = 0;
+                    placed[num] = false;                
+                    }
+            }
+        }
+        return false;
+    }
 }
