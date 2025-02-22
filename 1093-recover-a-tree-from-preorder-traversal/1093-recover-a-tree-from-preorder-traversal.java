3796a70eb5aee1fb52b1fad33b1cb7e7e4351f29
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-21 21:06:21 -0800

    Time: 7 ms (42.49%), Space: 44.9 MB (54.95%) - LeetHub

---
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+class Solution {
+    public TreeNode recoverFromPreorder(String s) {
+        Stack<TreeNode> st = new Stack<>();
+        int index = 0;
+        while(index<s.length()){
+            int depth = 0;
+            while(index < s.length() && s.charAt(index) == '-'){
+                depth++;
+                index++;
+            }
+            int val = 0;
+            while(index < s.length() && Character.isDigit(s.charAt(index))){
+                val = val*10 + (s.charAt(index)-'0');
+                index++;
+            }
+            TreeNode node = new TreeNode(val);
+            while(st.size() > depth){
+                st.pop();
+            }
+            if(!st.isEmpty()) {
+                if(st.peek().left ==null){
+                    st.peek().left = node;
+                }else{
+                    st.peek().right = node;
+                }
+            }
+            st.push(node);
+        }
+        while(st.size()>1){
+            st.pop();
+        }
+        return st.peek();
+    }
+}
