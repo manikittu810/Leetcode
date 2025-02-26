986f0d7a90bab778beed202a355ff84db48e6f1f
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-26 11:17:55 -0800

    Time: 2 ms (98.86%), Space: 45.2 MB (29.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -8,6 +8,6 @@ class Solution {
                 }
                 map.put(nums[i],i);
         }
-        return new int[]{-1,-1};
+        throw new IllegalArgumentException("No such output pair found");
     }
 }
