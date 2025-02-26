d9ce192069ff6f41957fa7fc044b0d5994406d51
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-25 16:05:14 -0800

    Time: 2 ms (88%), Space: 56.1 MB (27.47%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,15 +1,35 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] prefixProd = new int[nums.length];
-        prefixProd[0]=1;
+
+        int[] prefix = new int[nums.length];
+
+        prefix[0] = 1;
+
         for(int i=1;i<nums.length;i++){
-            prefixProd[i] = prefixProd[i-1]*nums[i-1];
+            prefix[i] = prefix[i-1]*nums[i-1];
         }
-        int suffix = 1;
-        for(int i=nums.length-1;i>=0;i--){
-            prefixProd[i] = prefixProd[i]*suffix;
-            suffix = suffix* nums[i];
+
+
+
+        int []suffix= new int[nums.length];
+
+        suffix[nums.length-1] = 1;
+
+
+
+        for(int i=nums.length-2;i>=0;i--){
+            suffix[i] = suffix[i+1] * nums[i+1];
         }
-        return prefixProd;
+
+        int []res = new int[nums.length];
+        for(int i=0;i<res.length;i++){
+            res[i] = suffix[i]*prefix[i];
+        }
+        return res;
     }
+    /*
+    [1,2,6,24]
+    [24,24,12,4]
+    
+     */
 }
