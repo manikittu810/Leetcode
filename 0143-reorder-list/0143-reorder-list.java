93927c42b3bd0afcee578242fe8c447b3ca380a1
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-27 15:17:41 -0800

    Time: 1 ms (99.8%), Space: 48.3 MB (42.62%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,45 +10,41 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if(head == null || head.next == null){
-            return ;
-        }
-        ListNode slow = head;
-        int count =0;
-        for(ListNode c=head;c!=null;c=c.next){
+        if(head ==null){return ;}
+        int count = 0;
+        for(ListNode cur = head;cur!=null; cur=cur.next){
             count++;
         }
-        for(int i=0;i<count/2;i++){
-            slow= slow.next;
-        }
-        
-        ListNode list2 = reverse(slow.next);
-        slow.next=null;
-        ListNode list1 = head;
-        while(list1!=null && list2!=null){
-            ListNode temp = list1.next;
-            list1.next = list2;
-            list1 = list1.next;
-            list2=list2.next;
-            list1.next = temp;
-            list1 = list1.next;
-        }
+        ListNode sptr = head;
+        for(int i =0;i<count/2;i++){
+            sptr = sptr.next;
+        } 
+        ListNode secondHalf = reverse(sptr.next);
+        sptr.next = null;
+        ListNode firstHalf = head;
+        while(firstHalf !=null && secondHalf!=null){
+            ListNode temp1 = firstHalf.next;
+            firstHalf.next = secondHalf;
+            firstHalf = firstHalf.next ;
+            secondHalf = secondHalf.next;
+            firstHalf.next = temp1;
+            firstHalf = firstHalf.next;
 
-    }
-    private ListNode reverse(ListNode head){
-        if(head ==null){
-            return null;
         }
+    }
+    public ListNode reverse(ListNode head){
+        if(head == null){return null;}
         ListNode cur = head;
         ListNode Next = null;
         ListNode prev = null;
-
-        while(cur!=null){
+        while(cur!=null)
+        {
             Next = cur.next;
             cur.next = prev;
             prev = cur;
             cur = Next;
         }
         return prev;
+
     }
 }
