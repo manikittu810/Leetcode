a24e11434d4057ca362e96c6d32a669599ac6aba
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-27 12:07:25 -0800

    Time: 3 ms (55.24%), Space: 44.6 MB (57.81%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,51 +1,42 @@
 class Solution {
     public boolean isValidSudoku(char[][] grid) {
-        
-        for(int i=0;i<9;i++){
-            Set<Character> set = new HashSet<>();
-            for(int j=0;j<9;j++){
-                if(grid[i][j] == '.') continue;
-                if(set.contains(grid[i][j])){ 
-                    return false;
-                }else{
-                    set.add(grid[i][j]);
-                }
-            }
-        }
+	   for(int i=0;i<9;i++){
+		Set<Character> set = new HashSet<>();
+		for(int j=0;j<9;j++){
+			if(set.contains(grid[i][j])){return false;}
+			if(grid[i][j]=='.'){continue;}else{
+				set.add(grid[i][j]);
+			}
+		}
+	}
 
-        for(int j=0;j<9;j++){
-            Set<Character> set = new HashSet<>();
-            for(int i=0;i<9;i++){
-                if(grid[i][j] == '.') continue;
-                if(set.contains(grid[i][j])){ 
-                    return false;
-                }else{
-                    set.add(grid[i][j]);
-                }
-            }
-        }
 
-        for(int smallGrid = 0;smallGrid < 9 ;smallGrid++){
-            Set<Character> smallGridSet = new HashSet<>();
-            int i = (smallGrid/3)*3;
-            int j =(smallGrid%3)*3;
-            for(int row=0;row<3;row++){
-                for(int col=0;col<3;col++){
-                    int newRow = i+row;
-                    int newCol = j+col;
-                    if(grid[newRow][newCol] == '.') {
-                        continue;
-                    }
-                    if(smallGridSet.contains(grid[newRow][newCol])) {
-                        return false;
-                    }else{
-                        smallGridSet.add(grid[newRow][newCol]);
-                    }
+	for(int j=0;j<9;j++){
+		Set<Character> set = new HashSet<>();
+		for(int i=0;i<9;i++){
+			if(set.contains(grid[i][j])){return false;}
+			if(grid[i][j]=='.'){continue;}else{
+				set.add(grid[i][j]);}
+		}
+	}
 
-                }
-            }
-        }
-    return true;
-
+	for(int smallGrid=0;smallGrid<9;smallGrid++){
+		Set<Character> smallGridSet = new HashSet<>();
+		int i = (smallGrid/3)*3;
+		int j = (smallGrid%3)*3;
+		for(int r = 0;r<3;r++){
+			for(int c = 0 ;c<3;c++){
+				int newRow = i+r;
+				int newCol = j+c;
+				if(smallGridSet.contains(grid[newRow][newCol])){
+					return false;
+				}
+				if(grid[newRow][newCol]=='.'){continue;}else{
+					smallGridSet.add(grid[newRow][newCol]);
+				}
+			}
+		}
+	}
+	return true;
     }
 }
