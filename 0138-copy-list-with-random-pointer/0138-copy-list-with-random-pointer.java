ddf050fc0744ff6187584e20a0164a2d7d5f27dd
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-27 17:02:57 -0800

    Time: 0 ms (100%), Space: 44.9 MB (11.47%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,40 +1,16 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
 class Solution {
     public Node copyRandomList(Node head) {
-        if(head == null){
-            return null;
-        }
-        Node cur = head;
-        Map<Node,Node> map = new HashMap<>();
-        while(cur!=null){
-            Node newNode = new Node(cur.val);
-            map.put(cur,newNode);
-            cur = cur.next;
-            newNode = newNode.next;
-        }
-        cur = head;
-        while(cur!=null){
-            Node node = map.get(cur);
-            node.next = map.get(cur.next);
-            node.random = map.get(cur.random);
-            node = node.next;
-            cur = cur.next;
-        }
-        return map.get(head);
-        
+	Map<Node,Node> map = new HashMap<>();
+	for(Node cur = head;cur!=null; cur = cur.next){
+		Node newNode = new Node(cur.val);
+		map.put(cur,newNode);
+	}        
+	
+	for(Node cur = head; cur!=null ; cur = cur.next){
+		Node newNode = map.get(cur);
+		newNode.next = map.get(cur.next);
+		newNode.random = map.get(cur.random);
+	}
+return map.get(head);
     }
 }
