be74ba3284d43dd0e9f5851ebd67ed88a26b94ab
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-27 19:20:51 -0800

    Time: 47 ms (55.3%), Space: 114 MB (59.34%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,67 +1,68 @@
-class LRUCache {
+class LRUCache{
+ class ListNode{
+  int key,val;
+  ListNode prev,next;
+  ListNode(int key,int val){
+  this.key = key;
+  this.val = val;
+  }
+ }
+ private ListNode head,tail;
+ private Map<Integer,ListNode> map;
+ private int capacity;
 
-	class ListNode{
-		int key,val;
-		ListNode prev,next;
-		ListNode(int key,int val){
-			this.key = key;
-			this.val = val;
-			}
-	}
+ public LRUCache(int capacity){
+  this.capacity = capacity;
+  map = new HashMap<>();
+  head = new ListNode(0,0);
+  tail = new ListNode(0,0);
+  head.next = tail;
+  tail.prev = head;
+ }
 
-	private Map<Integer,ListNode>map;
-	private int capacity;
-	private ListNode head,tail;
-    public LRUCache(int capacity) {
-        map = new HashMap<>();
-        head = new ListNode(0,0);
-        tail = new ListNode(0,0);
-	head.next = tail;
-	tail.prev = head;
-	this.capacity = capacity;
+ public int get(int key){
+  if(map.containsKey(key)){
+   ListNode node = map.get(key);
+   removeNode(node);
+   addToHead(node);
+  return  node.val;
+ }else{
+     return -1;
+  }
+ }
+
+ public void removeNode(ListNode node){
+  node.prev.next = node.next;
+  node.next.prev = node.prev;
+ }
+
+ public void addToHead(ListNode node){
+  node.next = head.next;
+  node.next.prev = node;
+  head.next = node;
+  node.prev = head;
+ }
+ public ListNode removeAtTail(){
+  ListNode lruNode = tail.prev;
+  removeNode(lruNode);
+  return lruNode;
+ }
+
+ public void put(int key,int value){
+ if(map.containsKey(key)){
+  ListNode node = map.get(key);
+  node.val = value;
+  removeNode(node);
+  addToHead(node);
+  map.put(key,node);
+  }else{
+    if(map.size() == capacity){
+     ListNode lru = removeAtTail();
+     map.remove(lru.key);
     }
-    
-    public int get(int key) {
-     if(map.containsKey(key)){
-    	ListNode node = map.get(key);
-	removeNode(node);
-	addToHead(node);
-	return node.val;
-     }    else{
-		return -1;
-	}
-    }
-	public void removeNode(ListNode node){
-		node.prev.next = node.next;
-		node.next.prev = node.prev;
-	}
-	public void addToHead(ListNode node){
-		node.next = head.next;
-		node.next.prev = node;
-		head.next = node;
-		node.prev = head;
-	}
-	public ListNode removeAtTail(){
-		ListNode lru = tail.prev;
-		removeNode(lru);
-		return lru;
-	}
-    
-    public void put(int key, int value) {
-	if(!map.containsKey(key)){
-        if(map.size() == capacity){
-            ListNode node = removeAtTail();
-            map.remove(node.key);
-        }
-	ListNode newNode = new ListNode(key,value);
-	addToHead(newNode);
-	map.put(key,newNode);
-	}else{
-	 ListNode node = map.get(key);
-	 removeNode(node);
-	 node.val = value;
-	 addToHead(node);
-	 map.put(key,node);	
-	}
+    ListNode newNode = new ListNode(key,value);
+    addToHead(newNode);
+    map.put(key,newNode);
    }
+ }
 }
