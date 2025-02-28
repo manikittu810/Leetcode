7e7795b62c5226da5a34f06f8c7138c6eda391e7
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-27 16:46:54 -0800

    Time: 0 ms (100%), Space: 41.7 MB (57.43%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,30 +1,17 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head == null){
-            return null;
-        }
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode ptr1 = dummy;
-        for(int i=0;i<n;i++){
-            ptr1=ptr1.next;
-        }
-        ListNode ptr2 = dummy;
-        while(ptr1.next!=null){
-            ptr1=ptr1.next;
-            ptr2=ptr2.next;
-        }
-        ptr2.next = ptr2.next.next;
-        return dummy.next;
+	ListNode dummy = new ListNode(-1);
+	dummy.next = head;
+	ListNode fptr = dummy;
+	for(int i=0;i<n;i++){
+		fptr = fptr.next; // reaches n steps ahead
+	}        
+	ListNode sptr = dummy;
+	while(fptr.next!=null){
+	fptr = fptr.next;
+	sptr = sptr.next;//finds the node before the node to be removed
+	}
+	sptr.next = sptr.next.next;
+	return dummy.next;
     }
 }
