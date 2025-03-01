b447298d511589c415775a4936b1e6eb0bd2abaf
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-02-28 18:06:50 -0800

    Time: 0 ms (100%), Space: 46.7 MB (27.51%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,27 @@
 class Solution {
     public int trap(int[] a) {
-	int l=0,r=a.length-1,ans = 0;
-	int maxLeftNow = a[l],maxRightNow = a[r];
-	while(l<r){
-		if(maxLeftNow <= maxRightNow){
-			l++;
-			maxLeftNow = Math.max(maxLeftNow,a[l]);
-			int n = maxLeftNow - a[l];
-			if(n>0){
-				ans+=n;
-			}
-		}
-		else{
-			if(maxLeftNow>maxRightNow){
-				r--;
-				maxRightNow = Math.max(maxRightNow,a[r]);
-				int n = maxRightNow - a[r];
-				if(n>0){
-					ans+=n;
-				}
-			}
-		}
-	}
-	return ans;
+        int l = 0, r=a.length-1,ans=0;
+        int maxLeftNow = a[l], maxRightNow = a[r];
+        while(l<r){
+            if(maxLeftNow <= maxRightNow){
+                l++;
+                maxLeftNow = Math.max(maxLeftNow,a[l]);
+                int n = maxLeftNow - a[l];
+                if(n>0){
+                    ans+=n;
+                }
+            }
+            else{
+                if(maxLeftNow>maxRightNow){
+                    r--;
+                    maxRightNow = Math.max(maxRightNow,a[r]);
+                    int n = maxRightNow - a[r];
+                    if(n>0){
+                        ans+=n;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 }
