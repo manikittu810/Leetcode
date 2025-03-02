6c86cfc64f64b83454486e18f7176782848fc3c5
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-03-02 13:46:11 -0800

    Time: 29 ms (72.6%), Space: 51.6 MB (50.41%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,23 +1,29 @@
-class Solution{
- public List<List<Integer>> threeSum(int[] a){
-  List<List<Integer>> list = new ArrayList<>();
-  Arrays.sort(a);
-  for(int i=0;i<a.length-2;i++){
-   if(i>0 && a[i] == a[i-1]) continue;
-   twoSum(a,a[i],i+1,list);
-  }
-  return list;
- }
- private void twoSum(int[]a,int curVal,int curIdx,List<List<Integer>> list){
-  int l=curIdx,r=a.length-1;
-  while(l<r){
-   int sum = curVal + a[l] + a[r];
-   if(sum==0){
-    list.add(Arrays.asList(curVal,a[l],a[r]));
-    while(l<r && a[l]==a[l+1]) l++;
-    while(l<r && a[r]==a[r-1]) r--;
-    l++; r--;
-   }else if(sum<0){l++;}else{r--;}
-  }
- }
+class Solution {
+    public List<List<Integer>> threeSum(int[] a) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(a);
+        for(int i =0;i<a.length-2;i++){
+            if(i>0 && a[i]==a[i-1]) continue;
+            twoSum(a,i+1,a[i],res);
+        }
+        return res;
+    }
+    private List<List<Integer>> twoSum(int[]a,int curIdx,int curNum,List<List<Integer>> res){
+        int l=curIdx,r = a.length-1;
+        while(l<r){
+            int sum = curNum+a[l]+a[r];
+            if(sum==0){
+                res.add(Arrays.asList(curNum,a[l],a[r]));
+                while(l<r && a[l] == a[l+1]) l++;
+                while(l<r && a[r] == a[r-1]) r--;
+                l++;
+                r--;
+            }else if(sum<0){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return res;
+    }
 }
