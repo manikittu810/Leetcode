29f40611eb78b685ff7431ffd222fc5d41ccc248
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-05-21 14:13:04 -0400

    Time: 2 ms (78.68%), Space: 44.3 MB (79.7%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,42 +1,38 @@
 class Solution {
-    public boolean isValidSudoku(char[][] grid) {
-	   for(int i=0;i<9;i++){
-		Set<Character> set = new HashSet<>();
-		for(int j=0;j<9;j++){
-			if(set.contains(grid[i][j])){return false;}
-			if(grid[i][j]=='.'){continue;}else{
-				set.add(grid[i][j]);
-			}
-		}
-	}
+    public boolean isValidSudoku(char[][] board) {
+        for(int i=0;i<9;i++){
+            Set<Character> set = new HashSet<>();
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.') continue;
+                if(set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
+        }
 
+        for(int j=0;j<9;j++){
+            Set<Character> set = new HashSet<>();
+            for(int i=0;i<9;i++){
+                if(board[i][j] == '.') continue;
+                if(set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
+        }
 
-	for(int j=0;j<9;j++){
-		Set<Character> set = new HashSet<>();
-		for(int i=0;i<9;i++){
-			if(set.contains(grid[i][j])){return false;}
-			if(grid[i][j]=='.'){continue;}else{
-				set.add(grid[i][j]);}
-		}
-	}
+        for(int smallGrid=0;smallGrid <9 ;smallGrid++){
+            Set<Character> set = new HashSet<>();
+            int i = (smallGrid/3) *3;
+            int j = (smallGrid%3) *3;
 
-	for(int smallGrid=0;smallGrid<9;smallGrid++){
-		Set<Character> smallGridSet = new HashSet<>();
-		int i = (smallGrid/3)*3;
-		int j = (smallGrid%3)*3;
-		for(int r = 0;r<3;r++){
-			for(int c = 0 ;c<3;c++){
-				int newRow = i+r;
-				int newCol = j+c;
-				if(smallGridSet.contains(grid[newRow][newCol])){
-					return false;
-				}
-				if(grid[newRow][newCol]=='.'){continue;}else{
-					smallGridSet.add(grid[newRow][newCol]);
-				}
-			}
-		}
-	}
-	return true;
+            for(int r = 0;r<3;r++){
+                for(int c=0;c<3;c++){
+                    int newRow = i+r;
+                    int newColumn = j+c;
+                    if(board[newRow][newColumn] =='.') continue;
+                    if(set.contains(board[newRow][newColumn])) return false;
+                    set.add(board[newRow][newColumn]);
+                }
+            }
+        }
+        return true;
     }
 }
