fa204903354e7a70240b5dab565bd4c457a4f0be
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-05-21 09:10:42 -0400

    Time: 4 ms (77.71%), Space: 43 MB (89.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,19 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {	
-        if(s.length()!=t.length()){return false;}
-		int []count = new int[26];
-		for(int i=0;i<s.length();i++){
-			count[s.charAt(i)-'a']++;		
-		}
-		for(int i=0;i<t.length();i++){
-			count[t.charAt(i)-'a']--;
-			if(count[t.charAt(i)-'a']<0){
-				return false;	
-			}
-		}
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        Map<Character,Integer> map = new HashMap<>();
+        int []count = new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            count[t.charAt(i)-'a']--;
+            if(count[t.charAt(i)-'a']<0){
+                return false;
+            }
+        }
         return true;
     }
 }
