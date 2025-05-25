1e146a47efa8691c86ae8f79eb7ac6590ebce7a9
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-05-25 01:00:57 -0400

    Time: 5 ms (75.35%), Space: 58.3 MB (13.8%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,13 +1,23 @@
 class Solution {
-    public int maxArea(int[] a) {
-        int l=0,r=a.length-1;int maxArea = 0;
-        while(l<r){
-            int len = r-l;
-            int breadth = Math.min(a[l],a[r]);
+    public int maxArea(int[] h) {
+        int i=0, j= h.length-1, max =0;
+        while(i<j){
+            int len = j-i;
+            int breadth = Math.min(h[i],h[j]);
             int area = len * breadth;
-            maxArea = Math.max(area,maxArea);
-            if(a[l]<a[r]){l++;}else{r--;}
+            max = Math.max (max,area);
+            if(h[i]<h[j]){
+                i++;
+            }else{
+                j--;
+            }
         }
-        return maxArea;
+        return max;
     }
 }
+
+/*
+height should be more and length should be more 
+height h[i] and also we can get the length from j-i;
+area = l*b;
+ */
