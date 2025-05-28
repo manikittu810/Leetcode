57c670b7ed78aa00bd30f7e2e34ae1255be1d7fd
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-05-28 09:36:24 -0400

    Time: 4 ms (49.73%), Space: 50.1 MB (30.58%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,39 +2,28 @@ class Solution {
     public int numIslands(char[][] grid) {
         int r = grid.length;
         int c = grid[0].length;
-        int count = 0;
+        int count =0;
         boolean [][]visited = new boolean[r][c];
-        Queue<int[]> q = new LinkedList<>();
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(grid[i][j] == '1'  && ! visited[i][j]){
-                    bfs(grid,visited,q,i,j,r,c);
+                if(!visited[i][j] && grid[i][j]=='1'){
+                    dfs(grid, visited,r,c,i,j);
                     count++;
                 }
-            }
+            } 
         }
         return count;
     }
-    private void bfs(char[][] grid, boolean[][]visited,
-    Queue<int[]> q,int i,int j,int r,int c){
-        visited[i][j]=true;
-        q.offer(new int[]{i,j});
-        int[][]directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        while(!q.isEmpty()){
-            int []a=q.poll();
-            for(int[]direction :directions){
-                int newRow = direction[0] + a[0];
-                int newCol = direction[1] +a[1];
-                if(newRow>=0 && newRow < r &&
-                newCol >= 0 && newCol<c &&
-                !visited[newRow][newCol] && 
-                grid[newRow][newCol]=='1'){
-                    visited[newRow][newCol] = true;
-                    q.offer(new int[]{newRow,newCol});
-                }
-            }
 
-        }
+private void dfs(char[][]grid, boolean[][]visited, int r,int c,int i, int j){
+        if(i<0 || j<0 || i>=r || j>=c || visited[i][j] || grid[i][j] == '0'){
+            return ;
+            }
+            visited[i][j] = true;
+            dfs(grid,visited,r,c,i+1,j);
+            dfs(grid,visited,r,c,i-1,j);
+            dfs(grid,visited,r,c,i,j+1);
+            dfs(grid,visited,r,c,i,j-1);
 
     }
 }
