579267464a903b391d7cb8131c2a2104bd4f5a3a
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-05-28 09:17:22 -0400

    Time: 6 ms (25.27%), Space: 51.9 MB (24.97%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,52 +1,40 @@
 class Solution {
     public int numIslands(char[][] grid) {
-
         int r = grid.length;
         int c = grid[0].length;
-
-        boolean [][]visited = new boolean[r][c];
-
-        Queue<int[]> queue = new LinkedList<>();
-
-        int[][]directions = {{1,0},{-1,0},{0,1},{0,-1}};
-
         int count = 0;
-
-
+        boolean [][]visited = new boolean[r][c];
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-
-                if(grid[i][j] == '1' && !visited[i][j]){
-
-                    visited[i][j] = true;
-
-                    queue.offer(new int[]{i,j});
-
+                if(grid[i][j] == '1'  && ! visited[i][j]){
+                    bfs(grid,visited,q,i,j,r,c);
                     count++;
-
-                        while(!queue.isEmpty()){
-
-
-                        int []u = queue.poll();
-
-                        for(int []direction : directions){
-
-                            int newRow = direction[0]+u[0];
-                            int newCol = direction[1]+u[1];
-
-                            if(newRow>=0 && newRow<r 
-                            && newCol>=0 && newCol<c
-                            && !visited[newRow][newCol] && grid[newRow][newCol] == '1'){
-
-                                visited[newRow][newCol] = true;
-                                queue.offer(new int[]{newRow,newCol});
-
-                            }
-                        }
-                    }
                 }
             }
         }
-        return count;   
+        return count;
+    }
+    private void bfs(char[][] grid, boolean[][]visited,
+    Queue<int[]> q,int i,int j,int r,int c){
+        visited[i][j]=true;
+        q.offer(new int[]{i,j});
+        int[][]directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.isEmpty()){
+            int []a=q.poll();
+            for(int[]direction :directions){
+                int newRow = direction[0] + a[0];
+                int newCol = direction[1] +a[1];
+                if(newRow>=0 && newRow < r &&
+                newCol >= 0 && newCol<c &&
+                !visited[newRow][newCol] && 
+                grid[newRow][newCol]=='1'){
+                    visited[newRow][newCol] = true;
+                    q.offer(new int[]{newRow,newCol});
+                }
+            }
+
+        }
+
     }
 }
