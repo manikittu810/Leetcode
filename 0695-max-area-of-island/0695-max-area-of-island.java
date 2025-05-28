39d2c04888cdb5704040a75a225d4d2d75bf3d68
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-05-28 11:45:22 -0400

    Time: 2 ms (70.83%), Space: 44.4 MB (81.8%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -2,38 +2,29 @@ class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int r = grid.length;
         int c = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
         boolean [][]visited = new boolean[r][c];
         int max = 0;
         for(int i=0;i<r;i++){
+            
             for(int j=0;j<c;j++){
                 if(!visited[i][j] && grid[i][j]==1){
-                    q.offer(new int[]{i,j});
-                    int area = bfs(grid,visited,i,j,r,c,q);
+                    int area = dfs(grid,visited,i,j,r,c);
                     max = Math.max(max,area);
                 }
             }
         }
         return max;
     }
-    private int bfs(int[][]grid, boolean [][]visited, int i , int j, int r, int c,Queue<int[]>q){
-        visited[i][j] = true;
-        int area = 1;
-        int[][]directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        while(!q.isEmpty()){
-            int []a = q.poll();
-            for(int[]direction : directions){
-                int newRow = direction[0] + a[0];
-                int newCol = direction[1] + a[1];
-                if(newRow>=0 && newRow<r &&
-                newCol>=0 && newCol<c &&
-                !visited[newRow][newCol] && grid[newRow][newCol]==1){
-                    visited[newRow][newCol] = true;
-                    q.offer(new int[]{newRow,newCol});
-                    area++;
-                }
-            }
+    private int dfs(int[][]grid, boolean [][]visited, int i , int j, int r, int c){
+        if(i<0 || i>=r || j<0 || j>=c || visited[i][j] || grid[i][j]!=1){
+            return 0;//0 beacuse there's no area here.
         }
-        return area;
+        visited[i][j] = true;
+        int area=1;
+         area+=dfs(grid,visited,i+1,j,r,c);
+         area+=dfs(grid,visited,i-1,j,r,c);
+         area+=dfs(grid,visited,i,j+1,r,c);
+         area+=dfs(grid,visited,i,j-1,r,c);
+         return area;
     }
 }
