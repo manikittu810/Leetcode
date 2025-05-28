f02977512c54593303ed795ca320d36a25e9c7a1
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-05-28 11:12:23 -0400

    Time: 4 ms (15.01%), Space: 45.2 MB (18.79%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,51 +1,47 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        int globalMax = 0;
+         int globalMax = 0;//assigned global value;
+        int r = grid.length;//row length
+        int c = grid[0].length;//column length
 
-
-        Queue<int[]> q = new LinkedList<>();
-        boolean [][]visited = new boolean[row][col];
-        int[][]directions = {
-            {0,-1},{0,1},{-1,0},{1,0}
-        };
-
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                visited[i][j] = true;
-
-                if(grid[i][j] == 1){
-                    
-
-                    int localMax = 1;
-
-                    q.offer(new int[]{i,j});
-
-                    while(!q.isEmpty()){
-
-                        int []u = q.poll();
-                        int celli = u[0];
-                        int cellj = u[1];
-
-                        for(int []dist: directions){
-                            int newRow = celli+dist[0];
-                            int newCol = cellj+dist[1];
-
-                            if(newRow>=0 && newRow<row &&
-                            newCol>=0 && newCol<col && 
-                            grid[newRow][newCol] == 1 
-                            &&!visited[newRow][newCol]){
-                            visited[newRow][newCol] = true;
-                            q.offer(new int[]{newRow,newCol});
-                            localMax++;
-                        }
-                    }
+        boolean [][]visited = new boolean[r][c];//visited boolean array for marking 
+        Queue<int[]> q = new LinkedList<>();//queue to hold data points where grid[i][j] == 1
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(!visited[i][j] && grid[i][j] == 1){// checked whether visited or not and checks whether the grid point is equal to 1.
+                    int area = bfs(grid,visited, i, j, r,c,q);// bfs
+                    globalMax = Math.max(globalMax,area);
                 }
-                globalMax = Math.max(localMax,globalMax);
             }
         }
-    }
         return globalMax;
-}
+    }
+    private int bfs(int[][]grid, boolean[][]visited, int i , int j, int r, int c, Queue<int[]> q){
+        visited[i][j] = true;
+        int area = 1;
+        q.offer(new int[]{i,j});// new (x,y) is added to the queue.
+        int[][]directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.isEmpty()){
+
+            int []a = q.poll();
+
+            for(int[] direction : directions){
+
+                int newRow = direction[0] + a[0];
+                int newCol = direction[1] + a[1];
+
+                if(
+                    newRow>=0 && newRow <r &&
+                    newCol >= 0 && newCol <c &&
+                    !visited[newRow][newCol] && 
+                    grid[newRow][newCol] == 1
+                ){
+                        visited[newRow][newCol] = true;
+                        q.offer(new int[]{newRow,newCol});
+                        area++;
+                }
+            }
+        }
+            return area;
+        }
 }
