d289c585e8b99f53f90ea72fb10f904cad5e92b8
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-06-05 08:10:56 -0400

    Time: 2 ms (68.43%), Space: 42.1 MB (93.2%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,60 +1,44 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        int freshOranges = 0;
-
+        int r = grid.length;
+        int c = grid[0].length;
+        int count = 0;
         Queue<int[]> q = new LinkedList<>();
-
-        for(int i=0;i<row;i++){
-            for(int j =0;j<col;j++){
-                if(grid[i][j] == 2){
+        for(int i=0;i<r;i++){
+            for(int j =0;j<c;j++){
+                if(grid[i][j]==2){
                     q.offer(new int[]{i,j});
-                }
-                else if(grid[i][j] == 1){
-                    freshOranges++;
-                }
+                }else if(
+                    grid[i][j] == 1
+                ){count++;}
             }
         }
-        if(freshOranges == 0){
-            return 0;//that means grid  contains no oranges(which are not fresh and not rot)
+        if(count == 0){
+            return 0;
         }
-
-        int time = 0;
-
-        int[][]directions = {
-            {1,0},
-            {-1,0},
-            {0,1},
-            {0,-1}
-        };
-
+        int time=0;
+        int[][]directions = {{1,0},{-1,0},{0,1},{0,-1}};
         while(!q.isEmpty()){
-            boolean freshToRot = false;
-            int n = q.size();//if you add q.size() at k<n in the below loop , it will behave as dynamic queue size updation which results in uneven calculations of time;
-            for(int k=0;k<n;k++){//here everytime this loop starts consider this as one unit of time
-                    int []u = q.poll();
-                    int i = u[0];
-                    int j = u[1];
-
-                    for(int[]direction : directions){
-                        int newRow = i + direction[0];
-                        int newColumn = j+direction[1];
-
-                        if(newRow>=0 &&
-                        newColumn>=0&&
-                        newRow<row&&
-                        newColumn<col&&
-                        grid[newRow][newColumn] == 1){
-                            grid[newRow][newColumn] = 2;
-                            q.offer(new int[]{newRow,newColumn});
-                            freshOranges--;
-                            freshToRot = true;
-                        }
-                    }
+            boolean something = false;
+            int n = q.size();
+            for(int i = 0;i<n;i++){
+            int []u = q.poll();
+            for(int []dir : directions){
+                int newRow = dir[0]+u[0];
+                int newCol = dir[1]+u[1];
+                if(newRow>=0 && newRow<r && newCol>=0 
+                && newCol<c && grid[newRow][newCol] == 1){
+                    grid[newRow][newCol] = 2;
+                    q.offer(new int[]{newRow,newCol});
+                    count--;
+                    something = true;
+                }
             }
-            if(freshToRot == true) time++;
+
         }
-        return (freshOranges == 0) ? time : -1;
+        if(something==true) time++;
+        }
+
+       return count == 0 ? time : -1;
     }
 }
