edcff9eece9355d5b5390d489a9af4dddb9c377b
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-06-06 07:13:51 -0400

    Time: 4 ms (97.82%), Space: 45.7 MB (60.02%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,51 +1,44 @@
 class Solution {
-    private int[][] directions = {
-        {0,1},{0,-1},{-1,0},{1,0}
-    };
-    public List<List<Integer>> pacificAtlantic(int[][] grid) {
+        private int [][]directions = {{1,0},{0,-1},{-1,0},{0,1}};
+    public List<List<Integer>> pacificAtlantic(int[][] height) {
+        List<List<Integer>> res = new ArrayList<>();
 
-        List<List<Integer>> res = new ArrayList<>(); 
+        int r = height.length;
+        int c = height[0].length;
+        boolean[][]pacific = new boolean[r][c];
+        boolean[][]atlantic = new boolean[r][c];
 
-        int row = grid.length;
-        int col = grid[0].length;
+        for(int i = 0;i<r;i++){
+            dfs(i,0,height,pacific);
+            dfs(i,c-1,height,atlantic);
 
-        boolean[][]pacific = new boolean[row][col];
-        boolean[][]atlantic = new boolean[row][col];
-
-        for(int i=0;i<row;i++){
-            dfs(i,0,pacific,grid);
-            dfs(i,col-1,atlantic,grid);
         }
 
-         for(int i=0;i<col;i++){
-            dfs(0,i,pacific,grid);
-            dfs(row-1,i,atlantic,grid);
+         for(int i = 0;i<c;i++){
+            dfs(0,i,height,pacific);
+            dfs(r-1,i,height,atlantic);
         }
 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(pacific[i][j] && atlantic[i][j]){
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(pacific[i][j]&& atlantic[i][j]){
                     res.add(Arrays.asList(i,j));
                 }
             }
         }
-return res;
+     return res;   
     }
-
-    private void dfs(int r ,int c, boolean[][]ocean,int[][]grid){
-        ocean[r][c] = true;
-        for(int[] i : directions){
-            int x = i[0]+r;
-            int y = i[1]+c;
-            if(
-                x>=0 && y>=0 &&
-                 x<grid.length && y<grid[0].length && 
-                !ocean[x][y] && 
-                grid[x][y] >= grid[r][c]
-            ){
-                    dfs(x,y,ocean,grid);
+    private void dfs(int i , int j, int [][]height,boolean[][] ocean){
+        ocean[i][j] = true;
+        for(int []dir : directions){
+            int newRow = dir[0] + i;
+            int newCol = dir[1] + j;
+            if(newRow>=0 && newCol>=0 
+            && newRow<height.length&&newCol<height[0].length  &&
+            !ocean[newRow][newCol]
+            && height[newRow][newCol] >= height[i][j]){
+                dfs(newRow,newCol,height,ocean);
             }
         }
-        
     }
 }
