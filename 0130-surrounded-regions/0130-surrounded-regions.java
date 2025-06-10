3117f200aaa05eb51cf94d1d497c383d41f69f34
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-06-09 22:49:03 -0400

    Time: 3 ms (33.04%), Space: 44.9 MB (99.41%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,57 +1,60 @@
 class Solution {
     private int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
     public void solve(char[][] board) {
-        if (board == null || board.length == 0 || board[0].length == 0) return;
+        if(board==null  || board.length==0 || board[0].length == 0) return ;
 
         int r = board.length;
         int c = board[0].length;
 
         Queue<int[]> q = new ArrayDeque<>();
-        markAndEnqueue(board,r,c,q);
-                    while(!q.isEmpty()){
-                        int []u = q.poll();
-                        for(int []dir : directions){
-                            int newRow = dir[0]+u[0];
-                            int newCol = dir[1] + u[1];
-                            if(newRow>=0 && newRow<r && newCol>=0 && newCol<c && 
-                            board[newRow][newCol]=='O'){
-                                board[newRow][newCol] = 'M'; 
-                                q.offer(new int[]{newRow,newCol});
-                            }
-                        }
-                    }
-                    for(int i=0;i<r;i++){
-                        for(int j=0;j<c;j++){
-                            if(board[i][j] == 'O'){
-                                board[i][j] = 'X';
-                            }else if(board[i][j] == 'M'){
-                                board[i][j] = 'O';
-                        }
-                        }
-                    }
-                            
+        markAndEnqueue(board, r, c, q);
+        while(!q.isEmpty()){
+            int []u = q.poll();
+            for(int []d : directions){
+                int newRow = d[0] +u[0];
+                int newCol = d[1] + u[1];
+                if(
+                    newRow>0 && newCol>0 && 
+                    newRow<r && newCol<c &&
+                    board[newRow][newCol]=='O'
+                ){
+                    board[newRow][newCol] = 'M';
+                    q.offer(new int[]{newRow,newCol});
+                }
             }
-    private void markAndEnqueue(char[][] board, int r, int c, Queue<int[]> q){
-        for(int i=0;i<r;i++){   
-            if(board[i][0] == 'O'){
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }else if(board[i][j] == 'M'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    private void markAndEnqueue(char[][]board,int r, int c,Queue<int[]>q){
+        for(int i=0;i<r;i++){
+            if(board[i][0]=='O'){
                 board[i][0] = 'M';
                 q.offer(new int[]{i,0});
             }
             if(board[i][c-1] == 'O'){
                 board[i][c-1] = 'M';
                 q.offer(new int[]{i,c-1});
+
             }
         }
+
         for(int j=0;j<c;j++){
-            if(board[r-1][j] == 'O'){
+            if(board[0][j] == 'O'){
+                board[0][j] ='M';
+                q.offer(new int[]{0,j});
+            }
+            if(board[r-1][j]=='O'){
                 board[r-1][j] = 'M';
                 q.offer(new int[]{r-1,j});
             }
-            if(board[0][j] == 'O'){
-                board[0][j] = 'M';
-                q.offer(new int[]{0,j});
-            }
         }
     }
-
 }
