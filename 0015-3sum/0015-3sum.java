22d5228fdd0ee5351a93163e35384934e51be0c5
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-01 18:28:21 -0400

    Time: 31 ms (57.02%), Space: 51.4 MB (83.03%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] a) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(a);// if i sort the array i can have chance to skip the duplicate elements
-        for(int i =0;i<a.length-2;i++){
-            if(i>0 && a[i]==a[i-1]) continue;
-            twoSum(a,a[i],i+1,list);
-        }
-        return list;
+            List<List<Integer>> res = new ArrayList<>();
+            Arrays.sort(a);//nlogn
+            for(int i=0;i<a.length;i++){
+                if(i>0 && a[i]==a[i-1]){continue;}
+                addSum(a,i,res);//nlogn+n
+            }
+            return res;
     }
-    public List<List<Integer>> twoSum(int[]a,int num, int j,List<List<Integer>> list){
-        int k = a.length-1;
-        while(j<k){ 
-            int sum = num + a[j]+a[k];
-            if(sum ==0){
-                list.add(Arrays.asList(num,a[j],a[k]));
-                while(j<k && a[j] == a[j+1]) j++;
-                while(j<k && a[k] == a[k-1]) k--;
-                j++;k--;
-           }
-           else if(sum<0){
-            j++;
-           }
-           else{
-            k--;
-           }
+    private void addSum(int[]a,int i,List<List<Integer>> l){
+        int j=i+1, k = a.length-1;
+        while(j<k){
+            
+            int sum = a[i]+a[j]+a[k];
+            if(sum==0){
+                l.add(Arrays.asList(a[i],a[j],a[k]));
+                j++;
+                k--;
+            while(j<k && a[j] == a[j-1]){j++;}
+            while(j<k && a[k] == a[k+1]){k--;}
+            }else if(sum<0){
+                j++;
+            }else if(sum>0){
+                k--;
+            }
+           
         }
-        return list;
     }
 }
