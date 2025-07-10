66aa7150e2dfd2a6534dcd9f5f71cee589420eb2
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-09 23:48:27 -0400

    Time: 0 ms (100%), Space: 46.5 MB (38.65%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,20 +1,41 @@
 class Solution {
-    public int trap(int[] a) {
-        int l=0,r= a.length-1;
-        int max_left_so_far = a[l];
-        int max_right_so_far = a[r];
-        int res =0;
+    public int trap(int[] h) {
+        int l=0,r=h.length-1;
+        int maxL=h[l],maxR=h[r], res=0;
         while(l<r){
-            if(max_left_so_far<=max_right_so_far){
+            if(maxL<=maxR){
                 l++;
-                max_left_so_far = Math.max(max_left_so_far,a[l]);
-                res+=max_left_so_far-a[l];
+                maxL=Math.max(maxL,h[l]);
+                res+=maxL-h[l];
             }else{
-                 r--;
-                max_right_so_far = Math.max(max_right_so_far,a[r]);
-                res+=max_right_so_far-a[r];
+                r--;
+                maxR=Math.max(maxR,h[r]);
+                res+=maxR-h[r];
             }
         }
         return res;
     }
 }
+/*
+each width is 1 unit
+l=0,
+r= n-1
+maxleftsofar = 0
+max right so far = 0
+res=0;
+while(l<r){
+    if(maxl<=maxr){
+        l++;
+        maxl= h[l];
+        res+=h[l]-maxl;//1-0=1;
+
+    }
+    else{
+        r--;
+        maxr= h[r];
+        res+=h[r]-maxr;
+    }
+    return res
+
+}
+ */
