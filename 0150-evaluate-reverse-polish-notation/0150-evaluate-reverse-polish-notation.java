43d156149f4a92d598c4a3e910bfa1287bcff82d
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-11 17:03:43 -0400

    Time: 6 ms (72.33%), Space: 44.8 MB (53.52%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,30 +1,20 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> st = new Stack<>();
-        for(String c : tokens){
-            if(c.equals("+") 
-            || c.equals("-") 
-            ||c.equals("*") 
-            ||c.equals("/") 
-            ){
+        for(String s : tokens){
+            if("+-*/".contains(s)){
                 int b = st.pop();
-                        int a = st.pop();
-                switch(c){
-
-                        
-
-                        case "+" : st.push(a+b);break;
-                        case "-" : st.push(a-b);break;
-                        case "*" : st.push(a*b);break;
-                        case "/" : st.push(a/b);break;
+                int a = st.pop();
+                switch(s){
+                    case "+" : st.push(a+b); break;
+                    case "-" : st.push(a-b); break;
+                    case "*" : st.push(a*b); break;
+                    case "/" : st.push(a/b); break;
                 }
             }else{
-                st.push(Integer.parseInt(c));
+                st.push(Integer.parseInt(s));
             }
         }
         return st.pop();
     }
 }
-/*
-
- */
