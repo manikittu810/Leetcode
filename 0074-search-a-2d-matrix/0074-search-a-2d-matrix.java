714433bba7cd75ddd55dc2306f3b761e0bb029c9
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-12 19:22:26 -0400

    Time: 0 ms (100%), Space: 42.2 MB (66.06%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,35 +2,34 @@ class Solution {
     public boolean searchMatrix(int[][] mat, int t) {
         int row = mat.length;
         int col = mat[0].length;
-        //to search 1st element in the each row and 
-        //also check whether the  falls inside the same 
-        //row by comparing the target with the last element of the same row
-        int top = 0,bottom = row-1;
-        while(top<=bottom){
-            int mid = top+(bottom-top)/2;
 
-            if(t>=mat[mid][0] && t<=mat[mid][col-1]){
-                return  bs(mat[mid],0,col-1,t);//this is where i'm stuck
-            }else if(t<mat[mid][0]){
-                bottom = mid-1;
+        int top = 0, bottom = row-1;
+
+        while(top<=bottom){
+            int mid = top +(bottom-top)/2;
+
+            if(t>=mat[mid][0] && t<= mat[mid][col-1]){
+                return bs(mat[mid], 0, col-1, t);
+            }else if(mat[mid][0]<t){
+                top=mid+1;
             }else{
-                top = mid+1;
+                bottom = mid-1;
             }
         }
-    return false;
-
+        return false;
     }
-    private boolean bs(int[]a,int i,int j,int t){
+    private boolean bs(int[]a, int i, int j, int t){
         while(i<=j){
-            int mid=i+(j-i)/2;
+            int mid = i+(j-i)/2;
             if(a[mid] == t){
                 return true;
-            }else if(a[mid] < t){
-                i=mid+1;
+            }else if(a[mid]<t){
+                i = mid+1;
             }else{
                 j=mid-1;
             }
         }
         return false;
     }
+    
 }
