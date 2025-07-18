b0c819750ea073d38fe978305d709303f26fdb97
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-18 11:00:29 -0400

    Time: 0 ms (100%), Space: 41.3 MB (47.51%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,17 +15,17 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        return isMirror(root);
-       
+        if(root == null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+        
     }
-private  static TreeNode isMirror(TreeNode root){
-    if(root == null){
-        return null;
-    }
-    TreeNode leftST = isMirror(root.left);
-    TreeNode rightST = isMirror(root.right);
-    root.left = rightST;
-    root.right = leftST;
-    return root;
-}
 }
