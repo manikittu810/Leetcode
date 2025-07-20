d89b2714e0edb1a1582372975e5bf8f61729adae
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-20 18:32:14 -0400

    Time: 1 ms (98.1%), Space: 41 MB (70.81%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.java
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -1,21 +1,19 @@
 class Solution {
-    public int lastStoneWeight(int[] a) {
-        if(a.length ==1){
-            return a[0];
-        }
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<>((Integer c,Integer b) -> b - c);
-        for(int i : a){
+    public int lastStoneWeight(int[] stones) {
+        if(stones.length==1){return stones[0];}
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((Integer b, Integer c) -> c-b);
+
+        for(int i : stones){
             maxHeap.offer(i);
         }
-
         while(maxHeap.size()>=2){
-            int x= maxHeap.poll();
-            int y = maxHeap.poll();
+            int x = maxHeap.poll();
+            int y= maxHeap.poll();
+
             if(x==y){
                 continue;
-            }
-            else{
-                maxHeap.offer(Math.abs(y-x));
+            }else{
+                maxHeap.offer(Math.abs(x-y));
             }
         }
         return (maxHeap.size()==1) ? maxHeap.peek() : 0;
