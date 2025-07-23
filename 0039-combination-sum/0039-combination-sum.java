eca35df0df290f06a229495517a658df78de9494
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-22 20:05:39 -0400

    Time: 3 ms (21.91%), Space: 44.6 MB (81.2%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,22 +2,15 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] a, int t) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> l = new ArrayList<>();
-        int sum = 0;
-        backTrack(a,t,res,l,0);
+        backTrack(a,t,0,res,l);
         return res;
     }
-    private void backTrack(int[]a,int t,List<List<Integer>> res,List<Integer> l, int i){
-        if(t==0){
-            res.add(new ArrayList<>(l));
-            return ;
-        }
-        if(t<0 || i>=a.length){
-            return ;
-        }
-            l.add(a[i]);
-            backTrack(a,t-a[i],res,l,i);
-            l.remove(l.size()-1);
-            backTrack(a,t,res,l,i+1);
-        
+    private void backTrack(int[]a,int t, int i, List<List<Integer>> res, List<Integer> l){
+        if(t==0){res.add(new ArrayList<>(l));return;}
+        if(t<0 || i>=a.length){return;}
+        l.add(a[i]);
+        backTrack(a,t-a[i],i,res,l);
+        l.removeLast();
+        backTrack(a,t,i+1,res,l);
     }
 }
