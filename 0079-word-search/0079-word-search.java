5fc33434092dcf78434c0b19a48811bb7d49616a
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-23 10:14:03 -0400

    Time: 141 ms (59.27%), Space: 41.5 MB (88.8%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,36 @@
+class Solution {
+    boolean [][]visited;
+    public boolean exist(char[][] a, String word) {
+        int n = a.length;
+        int m = a[0].length;
+       visited = new boolean[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(a[i][j] == word.charAt(0) && backTrack(i,j,0,a,word)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean backTrack(int i,int j,
+    int startIndex,char[][]a,String w){
+        if(w.length() == startIndex){
+            return true;
+        }
+        if(i<0 || i>=a.length || j<0||j>=a[i].length||visited[i][j]||a[i][j]!=w.charAt(startIndex)){
+            return false;
+        }
+        visited[i][j] = true;
+        if(
+            backTrack(i+1,j,startIndex+1,a,w) || 
+            backTrack(i-1,j,startIndex+1,a,w) || 
+            backTrack(i,j+1,startIndex+1,a,w) || 
+            backTrack(i,j-1,startIndex+1,a,w)
+        ){
+            return true;
+        }
+        visited[i][j] = false;
+        return false;
+    }
+}
