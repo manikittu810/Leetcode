d625048a6b81aa68af514496af5957ec2ed7aad2
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-23 10:28:34 -0400

    Time: 143 ms (55.84%), Space: 41.7 MB (61.66%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,31 +1,31 @@
 class Solution {
-    boolean [][]visited;
-    public boolean exist(char[][] a, String word) {
+    boolean[][]visited;
+    public boolean exist(char[][] a, String w) {
         int n = a.length;
         int m = a[0].length;
-       visited = new boolean[n][m];
+        visited = new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(a[i][j] == word.charAt(0) && backTrack(i,j,0,a,word)){
+                if(a[i][j] == w.charAt(0) && backTrack(i,j,0,a,w)){
                     return true;
                 }
             }
         }
         return false;
     }
-    private boolean backTrack(int i,int j,
-    int startIndex,char[][]a,String w){
+    private boolean backTrack(int i,int j, int startIndex, char[][]a,String w){
         if(w.length() == startIndex){
             return true;
         }
-        if(i<0 || i>=a.length || j<0||j>=a[i].length||visited[i][j]||a[i][j]!=w.charAt(startIndex)){
+        if(i<0 || i>=a.length || j<0 || j>=a[i].length||visited[i][j]||a[i][j]!=w.charAt(startIndex)){
             return false;
         }
+
         visited[i][j] = true;
         if(
-            backTrack(i+1,j,startIndex+1,a,w) || 
-            backTrack(i-1,j,startIndex+1,a,w) || 
-            backTrack(i,j+1,startIndex+1,a,w) || 
+            backTrack(i+1,j,startIndex+1,a,w) ||
+            backTrack(i-1,j,startIndex+1,a,w) ||
+            backTrack(i,j+1,startIndex+1,a,w) ||
             backTrack(i,j-1,startIndex+1,a,w)
         ){
             return true;
