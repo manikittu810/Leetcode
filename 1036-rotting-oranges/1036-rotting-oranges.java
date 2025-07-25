a5727fea93dae65a6a729370156c9c5d5a06e4b5
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-25 10:22:08 -0400

    Time: 1 ms (100%), Space: 42.3 MB (86.48%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,44 +1,43 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
-        int count = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        int freshOranges=0;
         Queue<int[]> q = new LinkedList<>();
-        for(int i=0;i<r;i++){
-            for(int j =0;j<c;j++){
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.offer(new int[]{i,j});
-                }else if(
-                    grid[i][j] == 1
-                ){count++;}
-            }
-        }
-        if(count == 0){
-            return 0;
-        }
-        int time=0;
-        int[][]directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        while(!q.isEmpty()){
-            boolean something = false;
-            int n = q.size();
-            for(int i = 0;i<n;i++){
-            int []u = q.poll();
-            for(int []dir : directions){
-                int newRow = dir[0]+u[0];
-                int newCol = dir[1]+u[1];
-                if(newRow>=0 && newRow<r && newCol>=0 
-                && newCol<c && grid[newRow][newCol] == 1){
-                    grid[newRow][newCol] = 2;
-                    q.offer(new int[]{newRow,newCol});
-                    count--;
-                    something = true;
+                }else if(grid[i][j]==1){
+                    freshOranges++;
                 }
             }
-
         }
-        if(something==true) time++;
-        }
+        int time =0;
+        int[][]directions = {{1,0},{-1,0},{0,-1},{0,1}};
+        while(!q.isEmpty()){
+            boolean isRotten = false;
 
-       return count == 0 ? time : -1;
+            int z = q.size();
+            for(int i=0;i<z;i++){
+                int[] cur = q.poll();
+            for(int[]d : directions){
+                int newRow = d[0]+cur[0];
+                int newCol = d[1]+cur[1];
+                if(newRow>=0 && newCol >=0 
+                && newRow < n && newCol < m 
+                && grid[newRow][newCol]==1){
+                    grid[newRow][newCol] =2;
+                    isRotten = true;
+                    freshOranges--;
+                    q.offer(new int[]{newRow,newCol});
+                }
+            }
+            }
+            if(isRotten == true){
+                time++;
+            }
+        }
+        return freshOranges==0 ? time : -1;
     }
 }
