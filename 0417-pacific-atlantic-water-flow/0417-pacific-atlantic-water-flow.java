53b88fc2b7593489fafcdd853fb241e8aa58adc0
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-25 14:03:15 -0400

    Time: 4 ms (97.17%), Space: 45.5 MB (71.76%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,21 +1,20 @@
 class Solution {
-    int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
-    public List<List<Integer>> pacificAtlantic(int[][] a) {
+    int[][]directions = {{1,0},{-1,0},{0,-1},{0,1}};
+    public List<List<Integer>> pacificAtlantic(int[][] h) {
         List<List<Integer>> res = new ArrayList<>();
-
-        int r = a.length;
-        int c = a[0].length;
-
+        int r= h.length;
+        int c= h[0].length;
         boolean[][]pacific = new boolean[r][c];
         boolean[][]atlantic = new boolean[r][c];
 
         for(int i=0;i<r;i++){
-            dfs(i,0,pacific,a);
-            dfs(i,c-1,atlantic,a);
+            dfs(i,0,pacific,h);
+            dfs(i,c-1,atlantic,h);
         }
-        for(int i = 0;i<c;i++){
-            dfs(0,i,pacific,a);
-            dfs(r-1,i,atlantic,a);
+
+        for(int i=0;i<c;i++){
+            dfs(0,i,pacific,h);
+            dfs(r-1,i,atlantic,h);
         }
 
         for(int i=0;i<r;i++){
@@ -27,19 +26,15 @@ class Solution {
         }
         return res;
     }
-    private void dfs(int r,int c, boolean[][]ocean, int[][]a){
-        ocean[r][c] = true;
-        for(int[]u:directions){
-            int newRow = u[0] + r;
-            int newCol = u[1] + c;
-
-            if(newRow>=0 && newCol>=0 &&newRow < a.length 
-            && newCol < a[0].length && !ocean[newRow][newCol] 
-            && a[newRow][newCol] >= a[r][c]){
-                dfs(newRow,newCol,ocean,a);
-            }
-
+    private void dfs(int i,int j, boolean[][]ocean,int[][]h){
+        ocean[i][j] = true;
+        for(int[]dir : directions){
+        int newRow = dir[0]+i;
+        int newCol = dir[1]+j;
+        if(newRow>=0 && newCol>=0 && newRow<h.length && newCol<h[0].length&&!ocean[newRow][newCol] && h[newRow][newCol]>=h[i][j]
+        ){
+            dfs(newRow,newCol,ocean,h);
         }
-        
+        }
     }
 }
