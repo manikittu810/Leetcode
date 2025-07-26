c2ac2e977a2d55ebb8f5031d1f6e8b316d4d24be
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-26 19:28:52 -0400

    Time: 7 ms (45.32%), Space: 45.6 MB (40.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -4,51 +4,32 @@ class Solution {
         for(int i=0;i<V;i++){
             l.add(new ArrayList<>());
         }
-        int[] inDegree = new int[V];
+        int[]inDegree = new int[V];
+
         for(int i=0;i<a.length;i++){
-            int u = a[i][1];
-            int v = a[i][0];
+            int v = a[i][1];
+            int u = a[i][0];
             l.get(u).add(v);
             inDegree[v]++;
         }
         Queue<Integer> q = new LinkedList<>();
 
         for(int i=0;i<V;i++){
-            if(inDegree[i]==0){
+            if(inDegree[i] == 0){
                 q.offer(i);
             }
         }
-
         int count = 0;
-
         while(!q.isEmpty()){
-
-            int  u = q.poll();
-
+            int n = q.poll();
             count++;
-
-            for(int v : l.get(u)){
-
-                inDegree[v]--;
-                
-                if(inDegree[v]==0){
-                    q.offer(v);
+            for(int u:l.get(n)){
+                inDegree[u]--;
+                if(inDegree[u]==0){
+                    q.offer(u);
                 }
-
             }
         }
         return (count!=V) ? false : true;
     }
 }
-/*
-Input: numCourses = 2, prerequisites = [[1,0]]
-Output: true
-Explanation: There are a total of 2 courses to take. 
-To take course 1 you should have finished course 0. So it is possible.
-Example 2:
-
-Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
-Output: false
-Explanation: There are a total of 2 courses to take. 
-To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
-*/
