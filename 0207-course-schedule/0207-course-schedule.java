4c6d62bd2e90b7407df48872ff183345e5c8146c
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-26 19:43:54 -0400

    Time: 7 ms (45.32%), Space: 45.2 MB (76.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -4,29 +4,30 @@ class Solution {
         for(int i=0;i<V;i++){
             l.add(new ArrayList<>());
         }
-        int[]inDegree = new int[V];
-
+        int []inDegree = new int[V];
         for(int i=0;i<a.length;i++){
             int v = a[i][1];
             int u = a[i][0];
             l.get(u).add(v);
             inDegree[v]++;
         }
+
         Queue<Integer> q = new LinkedList<>();
 
         for(int i=0;i<V;i++){
-            if(inDegree[i] == 0){
+            if(inDegree[i]==0){
                 q.offer(i);
             }
         }
+
         int count = 0;
         while(!q.isEmpty()){
-            int n = q.poll();
+            int u = q.poll();
             count++;
-            for(int u:l.get(n)){
-                inDegree[u]--;
-                if(inDegree[u]==0){
-                    q.offer(u);
+            for(int v : l.get(u)){
+                inDegree[v]--;
+                if(inDegree[v]==0){
+                    q.offer(v);
                 }
             }
         }
