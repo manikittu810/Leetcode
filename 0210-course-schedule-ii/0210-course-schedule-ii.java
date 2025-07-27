84cf69ecf358b4918a1e2d48518c7e8d505a3242
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-07-26 20:34:15 -0400

    Time: 5 ms (77.05%), Space: 46.5 MB (7.39%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,17 +1,23 @@
 class Solution {
     public int[] findOrder(int V, int[][] a) {
+
         List<List<Integer>> l = new ArrayList<>();
+
         for(int i=0;i<V;i++){
             l.add(new ArrayList<>());
         }
+
         int[]inDegree = new int[V];
+
         for(int i=0;i<a.length;i++){
             int v = a[i][1];
             int u = a[i][0];
-            l.get(u).add(v);
-            inDegree[v]++;
+            l.get(v).add(u);
+            inDegree[u]++;
         }
+
         Queue<Integer> q = new LinkedList<>();
+
         for(int i=0;i<V;i++){
             if(inDegree[i]==0){
                 q.offer(i);
@@ -19,10 +25,12 @@ class Solution {
         }
 
         int[]res = new int[V];
-        int idx = V-1;
+        int idx = 0;
+
         while(!q.isEmpty()){
+
             int u = q.poll();
-            res[idx--] = u;
+            res[idx++] = u;
             for(int v : l.get(u)){
                 inDegree[v]--;
                 if(inDegree[v]==0){
@@ -30,6 +38,6 @@ class Solution {
                 }
             }
         }
-        return res;
+        return (idx==V) ? res : new int[0];
     }
 }
