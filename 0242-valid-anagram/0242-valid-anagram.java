0df800aba27cb3d2074666bc018ed083ffff3861
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-10-06 22:55:39 -0400

    Time: 1 ms (99.58%), Space: 43.3 MB (66.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,13 +3,15 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int[] count = new int[26];
-        for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
+        int []count = new int[26];
+
+        for(char c : s.toCharArray()){
+            count[c-'a']++;
         }
-        for(int i=0;i<t.length();i++){
-            count[t.charAt(i)-'a']--;
-            if(count[t.charAt(i)-'a']<0){
+
+        for(char c : t.toCharArray()){
+            count[c-'a']--;
+            if(count[c-'a']<0){
                 return false;
             }
         }
