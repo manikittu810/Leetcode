3fc344ced8d54f36c945d7bda4b0baddb4ed7823
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-10-06 23:13:23 -0400

    Time: 6 ms (98.67%), Space: 48 MB (56.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,7 +5,7 @@ class Solution {
             String s1 = a[i];//eat
             char[] c = s1.toCharArray();//e,a,t
             Arrays.sort(c);//a,e,t
-            s1 = String.valueOf(c);//aet
+            s1 = new String(c);//aet
             if(!map.containsKey(s1)){
                 map.put(s1,new ArrayList<>());
             }
