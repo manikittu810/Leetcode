9f410977fd268224a849d209e6f25b03920cf6f9
Manikrishna <58840319+manikittu810@users.noreply.github.com>
2025-10-06 23:12:49 -0400

    Time: 6 ms (98.67%), Space: 48.1 MB (45.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,27 +1,16 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] s) {
+    public List<List<String>> groupAnagrams(String[] a) {
         Map<String, List<String>> map = new HashMap<>();
-        for(int i=0;i<s.length;i++){
-            String str = s[i];
-            char[]ch = str.toCharArray();
-            Arrays.sort(ch);
-            str = String.valueOf(ch);
-            if(!map.containsKey(str)){
-                map.put(str, new ArrayList<>());//[aet,[]]
+        for(int i=0;i<a.length;i++){
+            String s1 = a[i];//eat
+            char[] c = s1.toCharArray();//e,a,t
+            Arrays.sort(c);//a,e,t
+            s1 = String.valueOf(c);//aet
+            if(!map.containsKey(s1)){
+                map.put(s1,new ArrayList<>());
             }
-            map.get(str).add(s[i]);
+            map.get(s1).add(a[i]);
         }
         return new ArrayList<>(map.values());
     }
 }
-/*
-"eat"
-char[] ch = s[i].toCharArray();// ['e','a','t']
-sort the ch
-convert that sorted ch into a string again->key
-Map<String, List<String>> map ;
-<"aet",[]>
-if aet then add the current unmodified string from the string array into the existing sorted strings list in the map
-<"aet",[eat]>
-
- */
